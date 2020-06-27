a5009b627002fcb57bc531a398313f02c375c518
Ryan Skraba <ryan@skraba.com>
2020-06-27 10:16:48 +0200

    feat: Apply scalafmt

--- scalatags-by-example/src/main/scala/com/skraba/byexample/scalatags/palette/ColourSwatch.scala
+++ scalatags-by-example/src/main/scala/com/skraba/byexample/scalatags/palette/ColourSwatch.scala
@@ -5,32 +5,30 @@ import scalatags.Text.svgAttrs._
 import scalatags.Text.svgTags._
 
 /**
- * Provides a swatch for a colour in the palette, including lighter shades in a series of steps.
- *
- * The swatch is organised vertically with the title and RGB (in hex) at the top, and the shades at the bottom.
- *
- * @param name    The name of the colour
- * @param hex     The colour to use (i.e. "FF0000")
- * @param textHex The colour of the text naming the gradient.
- */
-case class ColourSwatch(name: String,
-                        hex: String,
-                        textHex: String) {
+  * Provides a swatch for a colour in the palette, including lighter shades in a series of steps.
+  *
+  * The swatch is organised vertically with the title and RGB (in hex) at the top, and the shades at the bottom.
+  *
+  * @param name    The name of the colour
+  * @param hex     The colour to use (i.e. "FF0000")
+  * @param textHex The colour of the text naming the gradient.
+  */
+case class ColourSwatch(name: String, hex: String, textHex: String) {
 
   /** @return a hex value that is f percent lighter than the actual colour. */
   def lightened(f: Double): String = ColourSwatch.lightened(hex, f)
 
   /**
-   * @param dx         The width of the swatch.
-   * @param dy         The height of the swatch.
-   * @param textSize   The font size of the title.
-   * @param textAdjust A helpful hint to push the text into a centered position.
-   * @param shades     The number of shades to apply on the swatch.
-   * @param shadeStep  The proportion to lighten per shade.
-   * @param shadeDy    The height of each shade colour.
-   * @param titleFont  The font to use for the colour name.
-   * @param shadeFont  The font to use for RGB values.
-   * @return the swatch for the given colour. */
+    * @param dx         The width of the swatch.
+    * @param dy         The height of the swatch.
+    * @param textSize   The font size of the title.
+    * @param textAdjust A helpful hint to push the text into a centered position.
+    * @param shades     The number of shades to apply on the swatch.
+    * @param shadeStep  The proportion to lighten per shade.
+    * @param shadeDy    The height of each shade colour.
+    * @param titleFont  The font to use for the colour name.
+    * @param shadeFont  The font to use for RGB values.
+    * @return the swatch for the given colour. */
   def toSvg(dx: Double = 50,
             dy: Double = 160,
             textSize: Double = 5,
@@ -41,8 +39,12 @@ case class ColourSwatch(name: String,
             titleFont: String = "Sans serif",
             shadeFont: String = "Sans serif"): Tag = {
 
-    val textTag: Tag = text(x := dx / 2, fill := s"#$textHex",
-      textAnchor := "middle", fontSize := textSize)
+    val textTag: Tag = text(
+      x := dx / 2,
+      fill := s"#$textHex",
+      textAnchor := "middle",
+      fontSize := textSize
+    )
 
     /** Preconfigured text tag for the name of this colour. */
     val titleTag: Tag = textTag(fontFamily := titleFont, fontWeight := "bold")
@@ -59,18 +61,22 @@ case class ColourSwatch(name: String,
     g(
       rect(y := 0, width := dx, height := dy, fill := s"#$hex"),
       titleTag(y := shadeDy / 2 + textAdjust)(name),
-      hexTag(y := shadeDy / 2 + textAdjust * 2 + textSize)(s"#${hex.toLowerCase}")
+      hexTag(y := shadeDy / 2 + textAdjust * 2 + textSize)(
+        s"#${hex.toLowerCase}"
+      )
     )(
       (1 to shades)
-        .map {
-          n =>
-            shade("#" + ColourSwatch.lightened(hex, shadeStep * n))(
-              transform := s"translate(0, ${dy - shadeDy * (shades - n + 1)})")
-        }: _*)
+        .map { n =>
+          shade("#" + ColourSwatch.lightened(hex, shadeStep * n))(
+            transform := s"translate(0, ${dy - shadeDy * (shades - n + 1)})"
+          )
+        }: _*
+    )
   }
 }
 
 object ColourSwatch {
+
   /** @return a hex value that is f percent lighter than the actual colour. */
   def lightened(hex: String, f: Double): String = {
     hex
--- scalatags-by-example/src/main/scala/com/skraba/byexample/scalatags/stamper/Stamper.scala
+++ scalatags-by-example/src/main/scala/com/skraba/byexample/scalatags/stamper/Stamper.scala
@@ -5,19 +5,19 @@ import scalatags.Text.svgAttrs.{height, width}
 import scalatags.Text.svgTags.rect
 
 /**
- * A Stamper draws SVG files by repeating an SVG element at different X/Y positions on the document.
- *
- * Create a Stamper with the SVG tag (configured without an x or y attribute).  A copy of the tag is "stamped" at the
- * origin, and every movement adds another to the list.  A complete history of tags can be obtained using [[asTags]].
- *
- * @param x           The x position for the first element added to the history.
- * @param y           The y position for the first element added to the history.
- * @param dx          Number of pixels to move in the x direction for a step.
- * @param dy          Number of pixels to move in the y direction for a step.
- * @param stamp       The SVG tag to reuse when moving.
- * @param history     The tags that have already been added during movement.
- * @param checkpoints A list of named checkpoint positions during movement.
- */
+  * A Stamper draws SVG files by repeating an SVG element at different X/Y positions on the document.
+  *
+  * Create a Stamper with the SVG tag (configured without an x or y attribute).  A copy of the tag is "stamped" at the
+  * origin, and every movement adds another to the list.  A complete history of tags can be obtained using [[asTags]].
+  *
+  * @param x           The x position for the first element added to the history.
+  * @param y           The y position for the first element added to the history.
+  * @param dx          Number of pixels to move in the x direction for a step.
+  * @param dy          Number of pixels to move in the y direction for a step.
+  * @param stamp       The SVG tag to reuse when moving.
+  * @param history     The tags that have already been added during movement.
+  * @param checkpoints A list of named checkpoint positions during movement.
+  */
 case class Stamper(x: Double = 0,
                    y: Double = 0,
                    dx: Double = 1,
@@ -27,59 +27,59 @@ case class Stamper(x: Double = 0,
                    checkpoints: Map[String, (Double, Double)] = Map()) {
 
   /**
-   * Move a relative number of pixels and add a copy of the stamp.  This does not take into account [[dx]] or [[dy]].
-   *
-   * @param dx    The number of pixels to move right.
-   * @param dy    The number of pixels to move left.
-   * @param stamp The stamp to use from now on (unchanged by default)
-   * @return An instance of the stamper at the new position coordinates.
-   */
+    * Move a relative number of pixels and add a copy of the stamp.  This does not take into account [[dx]] or [[dy]].
+    *
+    * @param dx    The number of pixels to move right.
+    * @param dy    The number of pixels to move left.
+    * @param stamp The stamp to use from now on (unchanged by default)
+    * @return An instance of the stamper at the new position coordinates.
+    */
   def mvRel(dx: Double = 0, dy: Double = 0, stamp: Tag = stamp): Stamper = {
     mvAbs(this.x + dx, this.y + dy, stamp)
   }
 
   /**
-   * Move to an abolute position and add a copy of the stamp.  This does not take into account [[dx]] or [[dy]].
-   *
-   * @param x     The new x position to add a stamp.
-   * @param y     The new y position to add a stamp.
-   * @param stamp The stamp to use from now on (unchanged by default)
-   * @return An instance of the stamper at the new position coordinates.
-   */
+    * Move to an abolute position and add a copy of the stamp.  This does not take into account [[dx]] or [[dy]].
+    *
+    * @param x     The new x position to add a stamp.
+    * @param y     The new y position to add a stamp.
+    * @param stamp The stamp to use from now on (unchanged by default)
+    * @return An instance of the stamper at the new position coordinates.
+    */
   def mvAbs(x: Double = 0, y: Double = 0, stamp: Tag = stamp): Stamper = {
     copy(x = x, y = y, stamp = stamp, history = asTags)
   }
 
   /**
-   * Move one unit north (up).
-   *
-   * @param stamp The stamp to use from now on (unchanged by default)
-   * @return An instance of the stamper at the new position.
-   */
+    * Move one unit north (up).
+    *
+    * @param stamp The stamp to use from now on (unchanged by default)
+    * @return An instance of the stamper at the new position.
+    */
   def n(stamp: Tag = stamp): Stamper = mvRel(dy = -dy, stamp = stamp)
 
   /**
-   * Move one unit south (down).
-   *
-   * @param stamp The stamp to use from now on (unchanged by default)
-   * @return An instance of the stamper at the new position.
-   */
+    * Move one unit south (down).
+    *
+    * @param stamp The stamp to use from now on (unchanged by default)
+    * @return An instance of the stamper at the new position.
+    */
   def s(stamp: Tag = stamp): Stamper = mvRel(dy = +dy, stamp = stamp)
 
   /**
-   * Move one unit east (right).
-   *
-   * @param stamp The stamp to use from now on (unchanged by default)
-   * @return An instance of the stamper at the new position.
-   */
+    * Move one unit east (right).
+    *
+    * @param stamp The stamp to use from now on (unchanged by default)
+    * @return An instance of the stamper at the new position.
+    */
   def e(stamp: Tag = stamp): Stamper = mvRel(dx = +dx, stamp = stamp)
 
   /**
-   * Move one unit west (left).
-   *
-   * @param stamp The stamp to use from now on (unchanged by default)
-   * @return An instance of the stamper at the new position.
-   */
+    * Move one unit west (left).
+    *
+    * @param stamp The stamp to use from now on (unchanged by default)
+    * @return An instance of the stamper at the new position.
+    */
   def w(stamp: Tag = stamp): Stamper = mvRel(dx = -dx, stamp = stamp)
 
   /** Move one unit northeast. */
@@ -94,7 +94,8 @@ case class Stamper(x: Double = 0,
   /** Move one unit southwest. */
   def sw(stamp: Tag = stamp): Stamper = mvRel(dx = -dx, dy = +dy, stamp = stamp)
 
-  def save(name: String): Stamper = copy(checkpoints = checkpoints + (name -> (x, y)))
+  def save(name: String): Stamper =
+    copy(checkpoints = checkpoints + (name -> (x, y)))
 
   def asTags: List[Tag] = {
     stamp(scalatags.Text.svgAttrs.x := x, scalatags.Text.svgAttrs.y := y) :: history
