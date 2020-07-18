8c650a783686fd5e2966f388dbd36d21c67dcf4a
Ryan Skraba <ryan@skraba.com>
2020-07-18 14:47:39 +0200

    feat: Add more SVG drawing methods to Stamper

--- scalatags-by-example/src/main/scala/com/skraba/byexample/scalatags/stamper/Stamper.scala
+++ scalatags-by-example/src/main/scala/com/skraba/byexample/scalatags/stamper/Stamper.scala
@@ -1,109 +1,214 @@
 package com.skraba.byexample.scalatags.stamper
 
 import scalatags.Text.implicits._
-import scalatags.Text.svgAttrs.{height, width}
-import scalatags.Text.svgTags.rect
+import scalatags.Text.svgTags.{g, rect, use}
+
+import scala.util.Random
 
 /**
-  * A Stamper draws SVG files by repeating an SVG element at different X/Y positions on the document.
+  * Draws SVG files by repeating an SVG tag at different X/Y positions on the document.
   *
-  * Create a Stamper with the SVG tag (configured without an x or y attribute).  A copy of the tag is "stamped" at the
-  * origin, and every movement adds another to the list.  A complete history of tags can be obtained using [[asTags]].
+  * Whenever the Stamper is moved, it leaves a copy of the tag at the current position before
+  * moving to the new position (as long as the pen is "down").
   *
   * @param x           The x position for the first element added to the history.
   * @param y           The y position for the first element added to the history.
-  * @param dx          Number of pixels to move in the x direction for a step.
-  * @param dy          Number of pixels to move in the y direction for a step.
-  * @param stamp       The SVG tag to reuse when moving.
+  * @param stepX       Number of pixels to move in the x direction for a step.
+  * @param stepY       Number of pixels to move in the y direction for a step.
+  * @param penDown     True to leave a trail as the stamper is moved.
+  * @param tag         The SVG tag to reuse when moving.
   * @param history     The tags that have already been added during movement.
   * @param checkpoints A list of named checkpoint positions during movement.
   */
 case class Stamper(
     x: Double = 0,
     y: Double = 0,
-    dx: Double = 1,
-    dy: Double = 1,
-    stamp: Tag = rect(width := 1, height := 1),
+    stepX: Double = 1,
+    stepY: Double = 1,
+    penDown: Boolean = true,
+    tag: Tag = rect(
+      scalatags.Text.svgAttrs.width := 1,
+      scalatags.Text.svgAttrs.height := 1
+    ),
     history: List[Tag] = Nil,
     checkpoints: Map[String, (Double, Double)] = Map()
 ) {
 
   /**
-    * Move a relative number of pixels and add a copy of the stamp.  This does not take into account [[dx]] or [[dy]].
+    * Move a relative number of pixels and add a copy of the stamp.  This does not take into
+    * account [[stepX]] or [[stepY]].
     *
     * @param dx    The number of pixels to move right.
     * @param dy    The number of pixels to move left.
-    * @param stamp The stamp to use from now on (unchanged by default)
+    * @param tag The tag to use from now on (unchanged by default)
     * @return An instance of the stamper at the new position coordinates.
     */
-  def mvRel(dx: Double = 0, dy: Double = 0, stamp: Tag = stamp): Stamper = {
-    mvAbs(this.x + dx, this.y + dy, stamp)
+  def stamp(dx: Double = 0, dy: Double = 0, tag: Tag = tag): Stamper = {
+    stampAt(this.x + dx, this.y + dy, tag)
   }
 
   /**
-    * Move to an abolute position and add a copy of the stamp.  This does not take into account [[dx]] or [[dy]].
+    * Move to an abolute position and add a copy of the stamp.  This does not take into
+    * account [[stepX]] or [[stepY]].
     *
     * @param x     The new x position to add a stamp.
     * @param y     The new y position to add a stamp.
-    * @param stamp The stamp to use from now on (unchanged by default)
+    * @param tag The stamp to use from now on (unchanged by default)
     * @return An instance of the stamper at the new position coordinates.
     */
-  def mvAbs(x: Double = 0, y: Double = 0, stamp: Tag = stamp): Stamper = {
-    copy(x = x, y = y, stamp = stamp, history = asTags)
+  def stampAt(x: Double = 0, y: Double = 0, tag: Tag = tag): Stamper = {
+    copy(x = x, y = y, tag = tag, history = appendCurrentToHistory)
   }
 
   /**
     * Move one unit north (up).
     *
-    * @param stamp The stamp to use from now on (unchanged by default)
+    * @param tag The stamp to use from now on (unchanged by default)
     * @return An instance of the stamper at the new position.
     */
-  def n(stamp: Tag = stamp): Stamper = mvRel(dy = -dy, stamp = stamp)
+  def n(tag: Tag = tag): Stamper = stamp(dy = -stepY, tag = tag)
 
   /**
     * Move one unit south (down).
     *
-    * @param stamp The stamp to use from now on (unchanged by default)
+    * @param tag The stamp to use from now on (unchanged by default)
     * @return An instance of the stamper at the new position.
     */
-  def s(stamp: Tag = stamp): Stamper = mvRel(dy = +dy, stamp = stamp)
+  def s(tag: Tag = tag): Stamper = stamp(dy = +stepY, tag = tag)
 
   /**
     * Move one unit east (right).
     *
-    * @param stamp The stamp to use from now on (unchanged by default)
+    * @param tag The stamp to use from now on (unchanged by default)
     * @return An instance of the stamper at the new position.
     */
-  def e(stamp: Tag = stamp): Stamper = mvRel(dx = +dx, stamp = stamp)
+  def e(tag: Tag = tag): Stamper = stamp(dx = +stepX, tag = tag)
 
   /**
     * Move one unit west (left).
     *
-    * @param stamp The stamp to use from now on (unchanged by default)
+    * @param tag The stamp to use from now on (unchanged by default)
     * @return An instance of the stamper at the new position.
     */
-  def w(stamp: Tag = stamp): Stamper = mvRel(dx = -dx, stamp = stamp)
+  def w(tag: Tag = tag): Stamper = stamp(dx = -stepX, tag = tag)
 
   /** Move one unit northeast. */
-  def ne(stamp: Tag = stamp): Stamper = mvRel(dx = +dx, dy = -dy, stamp = stamp)
+  def ne(tag: Tag = tag): Stamper = stamp(dx = +stepX, dy = -stepY, tag = tag)
 
   /** Move one unit southeast. */
-  def se(stamp: Tag = stamp): Stamper = mvRel(dx = +dx, dy = +dy, stamp = stamp)
+  def se(tag: Tag = tag): Stamper = stamp(dx = +stepX, dy = +stepY, tag = tag)
 
   /** Move one unit northwest. */
-  def nw(stamp: Tag = stamp): Stamper = mvRel(dx = -dx, dy = -dy, stamp = stamp)
+  def nw(tag: Tag = tag): Stamper = stamp(dx = -stepX, dy = -stepY, tag = tag)
 
   /** Move one unit southwest. */
-  def sw(stamp: Tag = stamp): Stamper = mvRel(dx = -dx, dy = +dy, stamp = stamp)
+  def sw(tag: Tag = tag): Stamper = stamp(dx = -stepX, dy = +stepY, tag = tag)
 
+  /**
+    * Save the current position of the stamper so it can be recalled later.
+    * @param name The name to associate with the current position.
+    */
   def save(name: String): Stamper =
     copy(checkpoints = checkpoints + (name -> (x, y)))
 
-  def asTags: List[Tag] = {
-    stamp(
-      scalatags.Text.svgAttrs.x := x,
-      scalatags.Text.svgAttrs.y := y
-    ) :: history
+  /**
+    * Using the position and [[stepX]] and [[stepY]] values, add the two dimension array of tags
+    * into the history of this stamper.
+    * @param pattern A two dimensional array of tags to be added to the history.  The tag at (0,0)
+    *                will be added at position [[x]], [[y]].
+    */
+  def draw(pattern: Iterable[Iterable[Option[Tag]]]): Stamper = {
+    val newTags: Iterable[Tag] = pattern
+      .map(_.zipWithIndex)
+      .zipWithIndex
+      .flatMap {
+        case (row: Iterable[(Option[Tag], Int)], rowIndex: Int) =>
+          row.map {
+            case (Some(tag), columnIndex) => Some(tag, rowIndex, columnIndex)
+            case _                        => None
+          }
+      }
+      .flatten
+      .map {
+        case (tag: Tag, rowIndex: Int, columnIndex: Int) =>
+          tag(
+            scalatags.Text.svgAttrs.x := x + stepX * columnIndex,
+            scalatags.Text.svgAttrs.y := y + stepY * rowIndex
+          )
+      }
+
+    copy(history = newTags.toList ++ history)
   }
 
+  /**
+    * Using the position and [[stepX]] and [[stepY]] values, add a two dimension array of tags
+    * into the history of this stamper.
+    *
+    * @param pattern A multiline string of characters representing a two dimension of tags.
+    * @param withTag A function mapping a character at a position in the string to a tag.
+    */
+  def draw(
+      pattern: String,
+      withTag: Char => Option[Tag] = useStampTag
+  ): Stamper = {
+    draw(pattern.split("\\n").toIterable.map(_.map(withTag).toIterable))
+  }
+
+  /**
+    * Clone the entire history of this stamper relative to the existing position.
+    *
+    * The returned stamper will have two elements in the history: the original grouped history and
+    * a clone at the new position.  The existing [[x]], [[y]] position will not be changed but
+    * the [[stepX]] and [[stepY]] parameters will be increased by the relative locations.
+    *
+    * @param id The SVG element id to use for the original history.
+    * @param dx Where to position the clone horizontally.
+    * @param dy Where to position the clone vertically.
+    */
+  def clone(
+      id: String = "id" + Random.alphanumeric.take(10).mkString,
+      dx: Double = stepX,
+      dy: Double = stepY
+  ): Stamper = {
+    copy(
+      stepX = stepX + dx,
+      stepY = stepY + dy,
+      history = List(
+        g(scalatags.Text.svgAttrs.id := s"$id")(history: _*),
+        use(
+          scalatags.Text.svgAttrs.xLinkHref := s"#$id",
+          scalatags.Text.svgAttrs.transform := s"translate($dx,$dy)"
+        )
+      )
+    )
+  }
+
+  /**
+    * Clone the entire history of this stamper to the south and the east without adding any
+    * additional tags or changing the position.  [[stepX]] and [[stepY]] are used to reposition the
+    * clones, and the resulting stamper will have double these values.
+    *
+    * @param id The SVG element id to use for the original history.
+    */
+  def clone4(
+      id: String = "id" + Random.alphanumeric.take(10).mkString,
+      dx: Double = stepX,
+      dy: Double = stepY
+  ): Stamper = {
+    clone(id, 0, dy).clone(id + "_2", dx, 0)
+  }
+
+  /** Helper method that maps any non-space character to the stamp tag. */
+  private[this] def useStampTag(c: Char): Option[Tag] =
+    if (c == ' ') None else Some(tag)
+
+  /** Helper method to add the tag to the history at the current position. */
+  private[this] def appendCurrentToHistory: List[Tag] = {
+    if (penDown)
+      tag(
+        scalatags.Text.svgAttrs.x := x,
+        scalatags.Text.svgAttrs.y := y
+      ) :: history
+    else history
+  }
 }
--- scalatags-by-example/src/test/scala/com/skraba/byexample/scalatags/stamper/StamperSpec.scala
+++ scalatags-by-example/src/test/scala/com/skraba/byexample/scalatags/stamper/StamperSpec.scala
@@ -15,64 +15,114 @@ class StamperSpec extends FunSpecLike with Matchers with BeforeAndAfterAll {
 
   describe("Using a Stamper") {
 
-    val base = rect(width := 1, height := 1)
-
-    it("draws two elements when moved") {
-      val origin = Stamper(stamp = rect)
+    it("draws one element when moved") {
+      val origin = Stamper(tag = rect)
       val east = origin.e()
 
       east.x shouldBe 1.0
       east.y shouldBe 0.0
 
-      east.asTags should have size 2
-      east.asTags.head shouldBe rect(x := 1.0, y := 0.0)
+      east.history should have size 1
+      east.history.head shouldBe rect(x := 0.0, y := 0.0)
 
       g(
-        east.asTags: _*
+        east.history: _*
+      ).render shouldBe """<g><rect x="0.0" y="0.0"></rect></g>"""
+    }
+
+    it("draws two elements when moved") {
+      val origin = Stamper(tag = rect)
+      val east = origin.e().e()
+
+      east.x shouldBe 2.0
+      east.y shouldBe 0.0
+
+      east.history should have size 2
+      east.history.head shouldBe rect(x := 1.0, y := 0.0)
+
+      g(
+        east.history: _*
       ).render shouldBe """<g><rect x="1.0" y="0.0"></rect><rect x="0.0" y="0.0"></rect></g>"""
     }
 
-    it("retains a checkpoint") {
+    it("draws no elements when the pen is up") {
+      val origin = Stamper(tag = rect, penDown = false)
+      val east = origin.e().e().e().e().e()
 
+      east.x shouldBe 5.0
+      east.y shouldBe 0.0
+
+      east.history shouldBe empty
+    }
+
+    it("retains a checkpoint") {
       val stamp =
-        Stamper(stamp = rect).se().save("1").se().e().se().se().save("2").se()
+        Stamper(tag = rect).se().save("1").se().e().se().se().save("2").se()
 
       stamp.checkpoints("1") shouldBe (1.0, 1.0)
       stamp.checkpoints("2") shouldBe (5.0, 4.0)
-      stamp.asTags.head shouldBe rect(x := 6.0, y := 5.0)
+      stamp.history should have size 6
+      stamp.history.head shouldBe rect(x := 5.0, y := 4.0)
     }
 
     it("should stamp in all redirections") {
-      val origin = Stamper(stamp = rect)
-      origin.n().asTags.head shouldBe rect(x := 0.0, y := -1.0)
-      origin.s().asTags.head shouldBe rect(x := 0.0, y := 1.0)
-      origin.e().asTags.head shouldBe rect(x := 1.0, y := 0.0)
-      origin.w().asTags.head shouldBe rect(x := -1.0, y := 0.0)
-      origin.ne().asTags.head shouldBe rect(x := 1.0, y := -1.0)
-      origin.nw().asTags.head shouldBe rect(x := -1.0, y := -1.0)
-      origin.se().asTags.head shouldBe rect(x := 1.0, y := 1.0)
-      origin.sw().asTags.head shouldBe rect(x := -1.0, y := 1.0)
+      val origin = Stamper(tag = rect)
+      origin.n().stamp().history.head shouldBe rect(x := 0.0, y := -1.0)
+      origin.s().stamp().history.head shouldBe rect(x := 0.0, y := 1.0)
+      origin.e().stamp().history.head shouldBe rect(x := 1.0, y := 0.0)
+      origin.w().stamp().history.head shouldBe rect(x := -1.0, y := 0.0)
+      origin.ne().stamp().history.head shouldBe rect(x := 1.0, y := -1.0)
+      origin.nw().stamp().history.head shouldBe rect(x := -1.0, y := -1.0)
+      origin.se().stamp().history.head shouldBe rect(x := 1.0, y := 1.0)
+      origin.sw().stamp().history.head shouldBe rect(x := -1.0, y := 1.0)
     }
 
     it("should stamp in all redirections with a custom distance and position") {
-      val origin = Stamper(x = 1, y = 2, dx = 3, dy = 4, stamp = rect)
-      origin.n().asTags.head shouldBe rect(x := 1.0, y := -2.0)
-      origin.s().asTags.head shouldBe rect(x := 1.0, y := 6.0)
-      origin.e().asTags.head shouldBe rect(x := 4.0, y := 2.0)
-      origin.w().asTags.head shouldBe rect(x := -2.0, y := 2.0)
-      origin.ne().asTags.head shouldBe rect(x := 4.0, y := -2.0)
-      origin.nw().asTags.head shouldBe rect(x := -2.0, y := -2.0)
-      origin.se().asTags.head shouldBe rect(x := 4.0, y := 6.0)
-      origin.sw().asTags.head shouldBe rect(x := -2.0, y := 6.0)
+      val origin = Stamper(x = 1, y = 2, stepX = 3, stepY = 4, tag = rect)
+      origin.n().stamp().history.head shouldBe rect(x := 1.0, y := -2.0)
+      origin.s().stamp().history.head shouldBe rect(x := 1.0, y := 6.0)
+      origin.e().stamp().history.head shouldBe rect(x := 4.0, y := 2.0)
+      origin.w().stamp().history.head shouldBe rect(x := -2.0, y := 2.0)
+      origin.ne().stamp().history.head shouldBe rect(x := 4.0, y := -2.0)
+      origin.nw().stamp().history.head shouldBe rect(x := -2.0, y := -2.0)
+      origin.se().stamp().history.head shouldBe rect(x := 4.0, y := 6.0)
+      origin.sw().stamp().history.head shouldBe rect(x := -2.0, y := 6.0)
     }
+
+    it("can clone itself into 4") {
+      val origin =
+        Stamper(x = 1, y = 2, stepX = 3, stepY = 4, tag = rect).stamp()
+      origin.history should have size 1
+
+      val cloned = origin.clone4("x")
+      cloned.stepX shouldBe 6.0
+      cloned.stepY shouldBe 8.0
+
+      // A southeast clone uses the history internally twice.
+      g(cloned.history: _*).render shouldBe
+        """<g>
+          |<g id="x_2">
+          |<g id="x"><rect x="1.0" y="2.0"></rect>
+          |</g>
+          |<use xlink:href="#x" transform="translate(0.0,4.0)"></use>
+          |</g>
+          |<use xlink:href="#x_2" transform="translate(3.0,0.0)"></use>
+          |</g>""".stripMargin.replaceAll("\n", "")
+    }
+  }
+
+  describe("Using a Stamper with examples") {
+
+    val base = rect(width := 1, height := 1)
 
     it("should create a couple of paths") {
       val lightGray = base(fill := "#EEE")
       val darkGray = base(fill := "#888")
       val pink = base(fill := "#F88")
-      val door = base(fill := "#FFF")
+      val door = base(fill := "#E8E8E8")
 
-      val circle = Stamper(stamp = darkGray)
+      // Draw a path in a circle.
+      val circle = Stamper(tag = darkGray)
         .w()
         .w()
         .w()
@@ -87,7 +137,7 @@ class StamperSpec extends FunSpecLike with Matchers with BeforeAndAfterAll {
         .s()
         .s()
         .s()
-        .save("eastWall")
+        .save("eastWall") // A remember a point in the path.
         .se()
         .s()
         .se()
@@ -113,11 +163,14 @@ class StamperSpec extends FunSpecLike with Matchers with BeforeAndAfterAll {
         .nw()
         .nw()
         .w()
+        .stamp()
 
+      // Get the position at the checkpoint.
       val eastWall = circle.checkpoints.getOrElse("eastWall", (0.0, 0.0))
 
+      // And create a new Stamper starting at the point.
       val extra =
-        Stamper(x = eastWall._1 - 1, y = eastWall._2, stamp = lightGray)
+        Stamper(x = eastWall._1 - 1, y = eastWall._2, tag = lightGray)
           .w()
           .sw()
           .s(door)
@@ -145,11 +198,165 @@ class StamperSpec extends FunSpecLike with Matchers with BeforeAndAfterAll {
           .n()
           .n()
           .n()
+          .stamp()
 
+      // Draw the SVG diagram.
       val plan =
-        g(circle.asTags: _*)(extra.asTags: _*)(transform := "translate(12,0)")
+        g(circle.history: _*)(extra.history: _*)(transform := "translate(12,0)")
 
-      Svg.toFile(File("/tmp/plan.svg"), plan, 100, 100);
+      Svg.toFile(File("/tmp/plan.svg"), plan)
+    }
+
+    it("should draw circles on a grid") {
+
+      val lightGray = base(fill := "#E8E8E8")
+      val darkGray = base(fill := "#888")
+      val pink = base(fill := "#FDD")
+
+      // A 128 x 128 checkered background
+      val background = g(
+        Stamper(tag = lightGray)
+          .se()
+          .stamp()
+          .copy(stepX = 2, stepY = 2)
+          .clone4()
+          .clone4()
+          .clone4()
+          .clone4()
+          .clone4()
+          .clone4()
+          .history: _*
+      )
+
+      // A pink dot in the middle of each 16x16 square
+      val foreground = g(
+        Stamper(x = 7, y = 7, stepX = 16, stepY = 16, tag = pink)
+          .stamp()
+          .clone4()
+          .clone4()
+          .clone4()
+          .history: _*
+      )
+
+      // Draw a path in a circle.
+      val layer1 = Stamper(5, 5, tag = darkGray)
+        .draw(""" XXX
+            |XXXXX
+            |XXXXX
+            |XXXXX
+            | XXX""".stripMargin)
+
+      val layer2 = Stamper(3, 3, tag = darkGray)
+        .draw("""  XXXXX
+            | XXXXXXX
+            |XXX   XXX
+            |XX     XX
+            |XX     XX
+            |XX     XX
+            |XXX   XXX
+            | XXXXXXX
+            |  XXXXX""".stripMargin)
+
+      val layer3 = Stamper(2, 2, tag = darkGray)
+        .draw("""   XXXXX
+            |  X     X
+            | X       X
+            |X         X
+            |X         X
+            |X         X
+            |X         X
+            |X         X
+            | X       X
+            |  X     X
+            |   XXXXX""".stripMargin)
+
+      val layer4 = Stamper(1, 1, tag = darkGray)
+        .draw("""    XXXXX
+            |   X     X
+            |  X       X
+            | X         X
+            |X           X
+            |X           X
+            |X           X
+            |X           X
+            |X           X
+            | X         X
+            |  X       X
+            |   X     X
+            |    XXXXX""".stripMargin)
+
+      val layer5 = Stamper(1, 1, tag = darkGray)
+        .draw("""   XXXXXXX
+            |  X       X
+            | X         X
+            |X           X
+            |X           X
+            |X           X
+            |X           X
+            |X           X
+            |X           X
+            |X           X
+            | X         X
+            |  X       X
+            |   XXXXXXX""".stripMargin)
+
+      val layer6 = Stamper(tag = darkGray)
+        .draw("""      XXX
+            |   XXX   XXX
+            |  X         X
+            | X           X
+            | X           X
+            | X           X
+            |X             X
+            |X             X
+            |X             X
+            | X           X
+            | X           X
+            | X           X
+            |  X         X
+            |   XXX   XXX
+            |      XXX""".stripMargin)
+
+      val layer7 = Stamper(tag = darkGray)
+        .draw("""     XXXXX
+            |   XX     XX
+            |  X         X
+            | X           X
+            | X           X
+            |X             X
+            |X             X
+            |X             X
+            |X             X
+            |X             X
+            | X           X
+            | X           X
+            |  X         X
+            |   XX     XX
+            |     XXXXX""".stripMargin)
+
+      // Draw the SVG diagram.
+      val plan =
+        g(
+          background,
+          g(layer1.history: _*)(transform := "translate(0,0)"),
+          g(layer2.history: _*)(transform := "translate(16,0)"),
+          g(layer3.history: _*)(transform := "translate(32,0)"),
+          g(layer4.history: _*)(transform := "translate(48,0)"),
+          g(layer5.history: _*)(transform := "translate(64,0)"),
+          g(layer6.history: _*)(transform := "translate(80,0)"),
+          g(layer7.history: _*)(transform := "translate(96,0)"),
+          g(layer7.history: _*)(transform := "translate(112,0)"),
+          g(layer7.history: _*)(transform := "translate(0,16)"),
+          g(layer6.history: _*)(transform := "translate(16,16)"),
+          g(layer5.history: _*)(transform := "translate(32,16)"),
+          g(layer4.history: _*)(transform := "translate(48,16)"),
+          g(layer3.history: _*)(transform := "translate(64,16)"),
+          g(layer2.history: _*)(transform := "translate(80,16)"),
+          g(layer1.history: _*)(transform := "translate(96,16)"),
+          foreground
+        )
+
+      Svg.toFile(File("/tmp/circles.svg"), plan, 128, 128)
     }
   }
 }
