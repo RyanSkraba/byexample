38c652e890b30e6642a407d87f597fbb6695c181
Ryan Skraba <ryan@skraba.com>
2021-02-12 19:56:35 +0100

    feat: Start adding tour of scala examples

---
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/tour/package.scala
@@ -0,0 +1,11 @@
+package com.skraba.byexample.scala
+
+/** @see https://docs.scala-lang.org/tour/tour-of-scala.html
+  */
+package object tour {
+  // You can put functions, classes, traits, etc. In a package object and they will be available
+  // everywhere in the package.
+
+  // https://alvinalexander.com/scala/scala-package-objects-how-to-name-location-cookbook-recipe
+  // recommends this style -- put it in x/y/z/package.scala, with package x.y and package object z
+}
