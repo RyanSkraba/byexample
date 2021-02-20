1add3752bffebc4eff2b4ce5e35e4d8d919fe711
Ryan Skraba <ryan@skraba.com>
2021-02-20 13:26:54 +0100

    feat: Add more scala tour

---
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/tour/Tour040Traits.scala
@@ -0,0 +1,72 @@
+package com.skraba.byexample.scala.tour
+
+import org.scalatest.funspec.AnyFunSpecLike
+import org.scalatest.matchers.should.Matchers
+
+/** Examples from the scala tour.
+  *
+  * @see https://docs.scala-lang.org/tour/tour-of-scala.html
+  */
+class Tour040Traits extends AnyFunSpecLike with Matchers {
+
+  describe("Traits") {
+
+    it("can be very simple.") {
+      trait HairColor
+
+      // Created an anonymous implementation
+      val x: HairColor = new HairColor {}
+      x shouldBe a[HairColor]
+    }
+
+    it("can be implemented with extends and generics") {
+      trait Iterator[A] {
+        def hasNext: Boolean
+
+        def next(): A
+      }
+
+      class IntIterator(to: Int) extends Iterator[Int] {
+        private var current = 0
+
+        override def hasNext: Boolean = current < to
+
+        override def next(): Int = {
+          if (hasNext) {
+            val t = current
+            current += 1
+            t
+          } else 0
+        }
+      }
+
+      val iterator = new IntIterator(10)
+      iterator.next() shouldBe 0
+      iterator.next() shouldBe 1
+
+    }
+
+    it("can be subtyped") {
+      import scala.collection.mutable.ArrayBuffer
+
+      trait Pet {
+        val name: String
+      }
+
+      class Cat(val name: String) extends Pet
+      class Dog(val name: String) extends Pet
+
+      val dog = new Dog("Harry")
+      val cat = new Cat("Sally")
+
+      val animals = ArrayBuffer.empty[Pet]
+      animals.append(dog)
+      animals.append(cat)
+
+      animals(0).name shouldBe "Harry"
+      animals(0) shouldBe a[Dog]
+      animals(1).name shouldBe "Sally"
+      animals(1) shouldBe a[Cat]
+    }
+  }
+}
---
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/tour/Tour060HigherOrderNestedCurryingSpec.scala
@@ -0,0 +1,83 @@
+package com.skraba.byexample.scala.tour
+
+import org.scalatest.funspec.AnyFunSpecLike
+import org.scalatest.matchers.should.Matchers
+
+/** Examples from the scala tour.
+  *
+  * @see https://docs.scala-lang.org/tour/tour-of-scala.html
+  */
+class Tour060HigherOrderNestedCurryingSpec
+    extends AnyFunSpecLike
+    with Matchers {
+
+  describe("Higher order functions") {
+
+    it("are instances and types like anything else") {
+      def apply(f: Int => String, v: Int) = f(v)
+
+      // Methods are automatically coerced to functions
+      val applyFunction: (Int => String, Int) => String = apply
+
+      // applyFunction shouldBe a[(Int => String, Int) => String]
+      applyFunction shouldBe a[(_ => _, _) => _]
+      apply("Hello %d".format(_), 10) shouldBe "Hello 10"
+      applyFunction("Hello %d".format(_), 10) shouldBe "Hello 10"
+
+      // Another example, uses generic type A.
+      class Decorator(left: String, right: String) {
+        def layout[A](x: A): String = left + x.toString + right
+      }
+
+      val decorator = new Decorator("[", "]")
+      apply(decorator.layout, 7) shouldBe "[7]"
+    }
+  }
+
+  describe("Nested methods") {
+    it("are possible") {
+      def factorial(x: Int): Int = {
+        def fact(x: Int, accumulator: Int): Int = {
+          if (x <= 1) accumulator
+          else fact(x - 1, x * accumulator)
+        }
+
+        fact(x, 1)
+      }
+
+      factorial(4) shouldBe 24
+      factorial(5) shouldBe 120
+    }
+  }
+
+  describe("Currying") {
+    it("can be applied to methods") {
+      def filter(xs: List[Int], p: Int => Boolean): List[Int] =
+        if (xs.isEmpty) xs
+        else if (p(xs.head)) xs.head :: filter(xs.tail, p)
+        else filter(xs.tail, p)
+
+      def modN(n: Int)(x: Int) = (x % n) == 0
+
+      filter((1 to 8).toList, modN(2)) shouldBe List(2, 4, 6, 8)
+      filter((1 to 8).toList, modN(3)) shouldBe List(3, 6)
+    }
+
+    it("can be applied to functions") {
+      // Must be lazy val because it refers to itself ?
+      lazy val filter: (List[Int], Int => Boolean) => List[Int] = { (xs, p) =>
+        {
+          if (xs.isEmpty) xs
+          else if (p(xs.head)) xs.head :: filter(xs.tail, p)
+          else filter(xs.tail, p)
+        }
+      }
+
+      // Note the difference in notation.
+      val modN: Int => Int => Boolean = n => x => (x % n) == 0
+
+      filter((1 to 8).toList, modN(2)) shouldBe List(2, 4, 6, 8)
+      filter((1 to 8).toList, modN(3)) shouldBe List(3, 6)
+    }
+  }
+}
