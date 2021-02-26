b04f543b32c9d2c415149975384764cd5c264cf9
Ryan Skraba <ryan@skraba.com>
2021-02-26 20:05:39 +0100

    test(scalatags): Add cheatsheet task unit tests

--- scala/scalatags-by-example/src/test/scala/com/skraba/byexample/scalatags/ScalatagsGoSpec.scala
+++ scala/scalatags-by-example/src/test/scala/com/skraba/byexample/scalatags/ScalatagsGoSpec.scala
@@ -5,7 +5,11 @@ import org.docopt.DocoptExitException
 import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatest.matchers.should.Matchers
 
-/** Unit tests for the helpers in the [[ScalatagsGo]] project.
+import java.io.ByteArrayOutputStream
+import java.nio.charset.StandardCharsets
+import scala.reflect.io.Streamable
+
+/** Unit tests for the CLI in the [[ScalatagsGo]] project.
   */
 class ScalatagsGoSpec extends AnyFunSpecLike with Matchers {
 
@@ -89,5 +93,60 @@ class ScalatagsGoSpec extends AnyFunSpecLike with Matchers {
       t.getMessage shouldBe "Unknown command: garbage"
       t.docopt shouldBe ScalatagsGo.Doc
     }
+  }
+}
+
+object ScalatagsGoSpec {
+
+  /** A helper method used to capture the console and apply it to a partial function.
+    * @param thunk code to execute that may use Console.out and Console.err print streams
+    * @param pf A partial function to apply matchers
+    * @tparam T The return value type of the thunk code to execute
+    * @tparam U The return value type of the partial function to return.
+    * @return The return value of the partial function.
+    */
+  def withConsoleMatch[T, U](
+      thunk: => T
+  )(pf: scala.PartialFunction[(T, String, String), U]): U = {
+    Streamable.closing(new ByteArrayOutputStream()) { out =>
+      Streamable.closing(new ByteArrayOutputStream()) { err =>
+        Console.withOut(out) {
+          Console.withErr(err) {
+            val t = thunk
+            Console.out.flush()
+            Console.err.flush()
+            // The return value
+            pf(
+              t,
+              new String(out.toByteArray, StandardCharsets.UTF_8),
+              new String(err.toByteArray, StandardCharsets.UTF_8)
+            )
+          }
+        }
+      }
+    }
+  }
+
+  /** A helper method used to capture the console of a ScalaGo execution and apply it to a partial function.
+    * @param args String arguments to pass to the ScalaGo.go method
+    * @param pf A partial function to apply matchers
+    * @tparam T The return value type of the thunk code to execute
+    * @tparam U The return value type of the partial function to return.
+    * @return The return value of the partial function.
+    */
+  def withScalatagsGoMatch[T, U](
+      args: String*
+  )(pf: scala.PartialFunction[(String, String), U]): U = {
+    withConsoleMatch(ScalatagsGo.go(args: _*)) { case (_, stdout, stderr) =>
+      pf(stdout, stderr)
+    }
+  }
+
+  /** A helper method used to capture the console of a ScalaGo execution and return the output.
+    * @param args String arguments to pass to the ScalaGo.go method
+    * @return A tuple of the stdout and stderr
+    */
+  def withScalatagsGo(args: String*): (String, String) = {
+    withScalatagsGoMatch(args: _*) { case any => any }
   }
 }
