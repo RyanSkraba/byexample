80c47f4cb939bf4c8ce195ab619a14ae5a7e5e27
Ryan Skraba <ryan@skraba.com>
2021-03-07 10:28:01 +0100

    feat(scala): Add CanBuildFrom with list state

--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/collections/CanBuildFromSpec.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/collections/CanBuildFromSpec.scala
@@ -4,8 +4,7 @@ import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatest.matchers.should.Matchers
 
 import scala.collection.generic.CanBuildFrom
-import scala.collection.{GenTraversableOnce, mutable}
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable
 
 /** Collections and [[CanBuildFrom]]
   *
@@ -85,16 +84,102 @@ class CanBuildFromSpec extends AnyFunSpecLike with Matchers {
       "val x: Seq[Boolean] = orig" should compile
 
       // This used to fail
-      orig.map(!_) shouldBe MagicList(false, true, false)
+      val mapped: MagicList = orig.map(!_)
+      mapped shouldBe MagicList(false, true, false)
 
       // MagicList can be used magically!
       orig ++ orig shouldBe MagicList(true, false, true, true, false, true)
+      orig ++ orig shouldBe a[MagicList]
       orig :+ false shouldBe MagicList(true, false, true, false)
       false +: orig shouldBe MagicList(false, true, false, true)
 
       // Because there's now a way to build it.
-      "implicitly[CanBuildFrom[MagicList, Boolean, MagicList]]" should compile
+      implicitly[CanBuildFrom[MagicList, Boolean, MagicList]] shouldBe mcbf
     }
   }
 
+  /** Another custom list implementation that adds state to the list.  This requires some
+    * fussing with [[equals()]] and [[hashCode()]] and should probably be avoided by preferring
+    * composition to inheritance.
+    *
+    * @param name the name of the list.
+    * @param dlg Delegate all of the list logic here.
+    */
+  case class NamedList(name: String, private val dlg: Boolean*)
+      extends Seq[Boolean] {
+    override def apply(idx: Int): Boolean = dlg(idx)
+
+    override def length: Int = dlg.length
+
+    override def iterator: Iterator[Boolean] = dlg.iterator
+
+    override def equals(that: Any): Boolean =
+      // Necessary because equals isn't generated since Seq already has an implementation.
+      that.isInstanceOf[NamedList] && that
+        .asInstanceOf[NamedList]
+        .name
+        .equals(name) && super.equals(that)
+
+    override def hashCode(): Int = name.hashCode * 31 + super.hashCode
+  }
+
+  object NamedList {
+
+    // Retain the name if building from a known collection.
+    implicit val SeqBooleanCanBuildFrom
+        : CanBuildFrom[Seq[Boolean], Boolean, NamedList] =
+      new CanBuildFrom[Seq[Boolean], Boolean, NamedList] {
+        override def apply(
+            from: Seq[Boolean]
+        ): mutable.Builder[Boolean, NamedList] =
+          new mutable.LazyBuilder[Boolean, NamedList] {
+            override def result(): NamedList = {
+              from match {
+                case nl: NamedList =>
+                  NamedList(nl.name, parts.flatMap(_.toSeq): _*)
+                case _ => NamedList("", parts.flatMap(_.toSeq): _*)
+              }
+
+            }
+          }
+
+        override def apply(): mutable.Builder[Boolean, NamedList] = apply(
+          NamedList("")
+        )
+      }
+  }
+
+  describe("NamedList") {
+
+    val orig: NamedList = NamedList("orig", true, false)
+
+    it("implements equals and hashCode") {
+      orig shouldBe NamedList("orig", true, false)
+      orig should not be NamedList("orig", false)
+      orig should not be NamedList("", true, false)
+      orig.hashCode should not be NamedList("", true, false).hashCode
+    }
+
+    it("can act like a sequence and NamedList") {
+
+      // This is still true.
+      "val x: Seq[Boolean] = orig" should compile
+
+      // This used to fail
+      orig.map(!_) shouldBe NamedList("orig", false, true)
+
+      val mapped: NamedList = orig.map(!_)
+      mapped shouldBe NamedList("orig", false, true)
+
+      // NamedList can be used magically!
+      import NamedList._
+      orig ++ orig shouldBe NamedList("orig", true, false, true, false)
+      orig :+ false shouldBe NamedList("orig", true, false, false)
+      false +: orig shouldBe NamedList("orig", false, true, false)
+
+      // But here's the weirdness and why you should avoid using lists with state.
+      orig should not be Seq(true, false)
+      Seq(true, false) shouldBe orig
+    }
+  }
 }
