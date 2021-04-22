01613ee24cd364b812aab60734c40f9ac53e315d
Ryan Skraba <ryan@skraba.com>
2021-04-22 09:44:42 +0200

    feat: Reorganise test code for command lines

--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/ScalaGoSpec.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/ScalaGoSpec.scala
@@ -1,10 +1,14 @@
 package com.skraba.byexample.scala
 
-import com.skraba.byexample.scala.ScalaGo.{withScalaGo, withScalaGoMatch}
+import com.skraba.byexample.scala.ScalaGoSpec.{withScalaGo, withScalaGoMatch}
 import org.docopt.DocoptExitException
 import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
+
+import java.io.ByteArrayOutputStream
+import java.nio.charset.StandardCharsets
+import scala.reflect.io.Streamable
 
 /** Unit tests for [[ScalaGo]]
   */
@@ -100,5 +104,60 @@ class ScalaGoSpec
       withScalaGo("--count", "3") shouldBe ("Hello, JavaScalaGo!\n" * 3, "")
       withScalaGo("--count=3") shouldBe ("Hello, JavaScalaGo!\n" * 3, "")
     }
+  }
+}
+
+object ScalaGoSpec {
+
+  /** A helper method used to capture the console and apply it to a partial function.
+    * @param thunk code to execute that may use Console.out and Console.err print streams
+    * @param pf A partial function to apply matchers
+    * @tparam T The return value type of the thunk code to execute
+    * @tparam U The return value type of the partial function to return.
+    * @return The return value of the partial function.
+    */
+  def withConsoleMatch[T, U](
+      thunk: => T
+  )(pf: scala.PartialFunction[(T, String, String), U]): U = {
+    Streamable.closing(new ByteArrayOutputStream()) { out =>
+      Streamable.closing(new ByteArrayOutputStream()) { err =>
+        Console.withOut(out) {
+          Console.withErr(err) {
+            val t = thunk
+            Console.out.flush()
+            Console.err.flush()
+            // The return value
+            pf(
+              t,
+              new String(out.toByteArray, StandardCharsets.UTF_8),
+              new String(err.toByteArray, StandardCharsets.UTF_8)
+            )
+          }
+        }
+      }
+    }
+  }
+
+  /** A helper method used to capture the console of a ScalaGo execution and apply it to a partial function.
+    * @param args String arguments to pass to the ScalaGo.go method
+    * @param pf A partial function to apply matchers
+    * @tparam T The return value type of the thunk code to execute
+    * @tparam U The return value type of the partial function to return.
+    * @return The return value of the partial function.
+    */
+  def withScalaGoMatch[T, U](
+      args: String*
+  )(pf: scala.PartialFunction[(String, String), U]): U = {
+    withConsoleMatch(ScalaGo.go(args: _*)) { case (_, stdout, stderr) =>
+      pf(stdout, stderr)
+    }
+  }
+
+  /** A helper method used to capture the console of a ScalaGo execution and return the output.
+    * @param args String arguments to pass to the ScalaGo.go method
+    * @return A tuple of the stdout and stderr
+    */
+  def withScalaGo(args: String*): (String, String) = {
+    withScalaGoMatch(args: _*) { case any => any }
   }
 }
