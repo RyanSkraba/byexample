4e8feed424b3dcc1a276df444b9373a641a73822
Ryan Skraba <ryan@skraba.com>
2021-07-17 15:25:00 +0200

    feat(lanterna): Add an example Java library

---
+++ misc/lanterna-by-example/src/main/java/com/skraba/byexample/misc/ClrsCc.java
@@ -0,0 +1,39 @@
+package com.skraba.byexample.misc;
+
+/** Prettier web colours from http://clrs.cc/ . */
+public class ClrsCc {
+
+  public static int[] NAVY = {0, 31, 63};
+
+  public static int[] BLUE = {0, 116, 217};
+
+  public static int[] AQUA = {127, 219, 255};
+
+  public static int[] TEAL = {57, 204, 204};
+
+  public static int[] OLIVE = {61, 153, 112};
+
+  public static int[] GREEN = {46, 204, 64};
+
+  public static int[] LIME = {1, 255, 112};
+
+  public static int[] YELLOW = {255, 220, 0};
+
+  public static int[] ORANGE = {255, 133, 27};
+
+  public static int[] RED = {255, 65, 54};
+
+  public static int[] MAROON = {133, 20, 75};
+
+  public static int[] FUCHSIA = {240, 18, 190};
+
+  public static int[] PURPLE = {177, 13, 201};
+
+  public static int[] BLACK = {17, 17, 17};
+
+  public static int[] GRAY = {170, 170, 170};
+
+  public static int[] SILVER = {221, 221, 221};
+
+  public static int[] WHITE = {255, 255, 255};
+}
