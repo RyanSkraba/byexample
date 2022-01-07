c52e0d2809b2331ae0379ac59e05b1660c03b9e4
Ryan Skraba <ryan@skraba.com>
2022-01-07 08:12:06 +0100

    feat(gtd): Add unit tests for updating weekly

--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/markd/GettingThingsDoneSpec.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/markd/GettingThingsDoneSpec.scala
@@ -1,6 +1,9 @@
 package com.skraba.byexample.scala.markd
 
-import com.skraba.byexample.scala.markd.GettingThingsDone.nextWeekStart
+import com.skraba.byexample.scala.markd.GettingThingsDone.{
+  H1Weekly,
+  nextWeekStart
+}
 import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatest.matchers.should.Matchers
 
@@ -11,11 +14,92 @@ import java.time.{DayOfWeek, LocalDate}
   */
 class GettingThingsDoneSpec extends AnyFunSpecLike with Matchers {
 
-  describe("Updating the weekly status") {}
+  describe(s"Updating the $H1Weekly section") {
+
+    /** Some text to be parsed into an status document. */
+    val original = s"""
+       |# H1 One
+       |1
+       |# H1 Two
+       |2
+       |# H1 Three
+       |3""".stripMargin
+
+    it("should add itself to an empty document") {
+      val empty = GettingThingsDone(text = "")
+      val updated =
+        empty.updateH1Weekly(weekly => weekly.copyMds(Seq(Comment("Hello"))))
+      updated.doc shouldBe Header(0, "", Header(1, H1Weekly, Comment("Hello")))
+    }
+
+    it(s"should add itself document where the section doesn't exist") {
+      val existing = GettingThingsDone(text = original)
+      val updated =
+        existing.updateH1Weekly(weekly => weekly.copyMds(Seq(Comment("Hello"))))
+      updated.doc shouldBe Header(
+        0,
+        "",
+        Header(1, "H1 One", Paragraph("1")),
+        Header(1, "H1 Two", Paragraph("2")),
+        Header(1, "H1 Three", Paragraph("3")),
+        Header(1, H1Weekly, Comment("Hello"))
+      )
+    }
+
+    describe("should update an existing section") {
+      it("at the start") {
+        val existing =
+          GettingThingsDone(text = original.replace("H1 One", H1Weekly))
+        val updated =
+          existing.updateH1Weekly(weekly =>
+            weekly.copyMds(weekly.mds :+ Comment("Hello"))
+          )
+        updated.doc shouldBe Header(
+          0,
+          "",
+          Header(1, H1Weekly, Paragraph("1"), Comment("Hello")),
+          Header(1, "H1 Two", Paragraph("2")),
+          Header(1, "H1 Three", Paragraph("3"))
+        )
+      }
+
+      it("in the middle") {
+        val existing =
+          GettingThingsDone(text = original.replace("H1 Two", H1Weekly))
+        val updated =
+          existing.updateH1Weekly(weekly =>
+            weekly.copyMds(weekly.mds :+ Comment("Hello"))
+          )
+        updated.doc shouldBe Header(
+          0,
+          "",
+          Header(1, "H1 One", Paragraph("1")),
+          Header(1, H1Weekly, Paragraph("2"), Comment("Hello")),
+          Header(1, "H1 Three", Paragraph("3"))
+        )
+      }
+
+      it("at the end") {
+        val existing =
+          GettingThingsDone(text = original.replace("H1 Three", H1Weekly))
+        val updated =
+          existing.updateH1Weekly(weekly =>
+            weekly.copyMds(weekly.mds :+ Comment("Hello"))
+          )
+        updated.doc shouldBe Header(
+          0,
+          "",
+          Header(1, "H1 One", Paragraph("1")),
+          Header(1, "H1 Two", Paragraph("2")),
+          Header(1, H1Weekly, Paragraph("3"), Comment("Hello"))
+        )
+      }
+    }
+  }
 
   describe("Utility for calculating a new week") {
 
-    it(s"should ignore suffixes") {
+    it("should ignore suffixes") {
       nextWeekStart(
         Some("2021/09/05 Extra")
       ) shouldBe "2021/09/06"
