ab91f7316556a387020f469240999d0efce5543a
Ryan Skraba <ryan@skraba.com>
2022-01-13 08:06:09 +0100

    doc(gtd): Update scaladoc

--- scala/scala-by-example/src/main/scala/com/skraba/byexample/scala/markd/GettingThingsDone.scala
+++ scala/scala-by-example/src/main/scala/com/skraba/byexample/scala/markd/GettingThingsDone.scala
@@ -4,7 +4,63 @@ import com.skraba.byexample.scala.markd.GettingThingsDone._
 
 import java.time.DayOfWeek
 
-/** Backing class for the getting_things_done.sc script. */
+/** A markdown document that helps organising yourself.
+  *
+  * The document can contain any number of headers and sections using the [[Markd]] model, but
+  * provides some additional semantics.
+  *
+  * The {{{getting_things_done.sc}}} ammonite script uses this class to provide a CLI.
+  *
+  * =Weekly Status=
+  *
+  * A top level (Header 1) section that contains a subsection for each week. Some methods in
+  * this class are used to update the latest or a specific week.
+  *
+  * {{{
+  * Weekly Status
+  * ==============================================================================
+  *
+  * 2021/09/13
+  * ------------------------------------------------------------------------------
+  *
+  * <... weekly status reported here ...>
+  *
+  * 2021/09/6
+  * ------------------------------------------------------------------------------
+  *
+  * <... weekly status reported here ...>
+  * }}}
+  *
+  * =Statistic tables=
+  *
+  * In a weekly status, you can have per-day statistics in a condensed table.  Helper methods
+  * make it easy to add a new row or update a value for today.
+  *
+  * {{{
+  * 2021/09/13
+  * ------------------------------------------------------------------------------
+  *
+  * | Stats  | Mon | Tue | Wed | Thu | Fri | Sat | Sun |
+  * |--------|-----|-----|-----|-----|-----|-----|-----|
+  * | unread |     |     |     | 449 |     |     |     |
+  * }}}
+  * =To Do task tables=
+  *
+  * In a weekly status, you can list tasks in a table, along with a status.
+  *
+  * {{{
+  * 2021/09/13
+  * ------------------------------------------------------------------------------
+  *
+  * | To Do      | Notes                                                                     |
+  * |------------|---------------------------------------------------------------------------|
+  * | 🟢Tech     | **Do the thing** Notes on how it was done                                 |
+  * | 🔶Personal | **Maybe doable** Or paused, or to think about for next week, or in danger |
+  * | 🟥Health   | **Not done** Here's why                                                   |
+  * | ⤴️Personal | **Read Getting Things Done Chapter 4/12** Moved to next week              |
+  * | Pro        | **Another task** With some [details][20210913-1]                          |
+  * }}}
+  */
 case class GettingThingsDone(doc: Header) {
 
   /** Helper function to update only the weekly statuses section of the document, adding
@@ -37,15 +93,15 @@ case class GettingThingsDone(doc: Header) {
     }
 
   /** Update a statistics table in the top week.
-    * @param rowStat The name of the row to update in the statistics table.
+    * @param row The name of the row to update in the statistics table.
     * @param cell The new value for that statistic.
-    * @param colStat The day to apply that new value to.
+    * @param col The day to apply that new value to.
     * @return This document with the statistics updated.
     */
   def updateTopWeekStats(
-      rowStat: String,
+      row: String,
       cell: String,
-      colStat: Option[String] = None
+      col: Option[String] = None
   ): GettingThingsDone = updateTopWeek { weekly =>
     import java.time.LocalDate
     weekly.mapFirstIn(ifNotFound = TableStatsEmpty +: weekly.mds) {
@@ -53,10 +109,10 @@ case class GettingThingsDone(doc: Header) {
       case tb @ Table(_, Seq(TableRow(Seq(a1: String, _*)), _*))
           if a1 == TableStats =>
         val statsRow =
-          tb.mds.indexWhere(_.cells.headOption.contains(rowStat))
-        val row = if (statsRow != -1) statsRow else tb.mds.size
+          tb.mds.indexWhere(_.cells.headOption.contains(row))
+        val nRow = if (statsRow != -1) statsRow else tb.mds.size
 
-        val statsCol = colStat
+        val statsCol = col
           .map(c =>
             tb.mds.headOption
               .map(_.cells.indexWhere(_ == c))
@@ -65,13 +121,13 @@ case class GettingThingsDone(doc: Header) {
           .getOrElse(
             LocalDate.now.getDayOfWeek.getValue
           )
-        val col =
+        val nCol =
           if (statsCol != -1) statsCol
           else LocalDate.now.getDayOfWeek.getValue
 
         if (statsRow == -1)
-          tb.updated(col, row, cell).updated(0, row, rowStat)
-        else tb.updated(col, row, cell)
+          tb.updated(nCol, nRow, cell).updated(0, nRow, row)
+        else tb.updated(nCol, nRow, cell)
     }
   }
 
@@ -98,6 +154,7 @@ case class GettingThingsDone(doc: Header) {
 
 object GettingThingsDone {
 
+  /** The structure of an empty Stats table, used to collect weekly statistics. */
   lazy val TableStatsEmpty: Table = Table.from(
     Seq.fill(8)(Align.LEFT),
     TableRow.from(
@@ -112,6 +169,7 @@ object GettingThingsDone {
     )
   )
 
+  /** The structure of an empty To Do table, used to collect stats. */
   lazy val TableToDoEmpty: Table = Table.from(
     Seq(Align.LEFT, Align.LEFT),
     TableRow.from("To Do", "Notes")
@@ -122,6 +180,12 @@ object GettingThingsDone {
   val TableStats: String = TableStatsEmpty.mds.head.cells.head
   val TableToDo: String = TableToDoEmpty.mds.head.cells.head
 
+  /** Create an instance from a markdown string.
+    *
+    * @param content The string contents
+    * @param cfg
+    * @return
+    */
   def apply(
       content: String,
       cfg: ParserCfg = new ParserCfg()
