8f12ab7a6e49404d3dd6ebb507c91705a2a0752b
Ryan Skraba <ryan@skraba.com>
2022-03-10 19:03:04 +0100

    feat(testing): Add CharSequence tests

--- testing/assertj-by-example/src/test/java/com/skraba/byexample/assertj/StringTest.java
+++ testing/assertj-by-example/src/test/java/com/skraba/byexample/assertj/StringTest.java
@@ -1,5 +1,6 @@
 package com.skraba.byexample.assertj;
 
+import java.nio.CharBuffer;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,25 @@ class StringTest {
   void testBasicString() {
     String issue = "ABC-1234";
     Assertions.assertThat(issue)
+        .isNotBlank()
+        .isEqualTo("ABC-1234")
+        .contains("C-1")
+        .startsWith("ABC")
+        .endsWith("1234")
+        .matches("[A-Z]+-\\d+");
+  }
+
+  @Test
+  void testCharSequence() {
+    // Note that all CharSequence with the same contents aren't necessarily equal!
+    CharSequence issue = CharBuffer.wrap("ABC-1234");
+
+    // The CharBuffer is not equal to the "same" String
+    Assertions.assertThat(issue).isNotEqualTo("ABC-1234");
+
+    // Unless you specify the Comparator explicitly.
+    Assertions.assertThat(issue)
+        .usingComparator(CharSequence::compare)
         .isNotBlank()
         .isEqualTo("ABC-1234")
         .contains("C-1")
