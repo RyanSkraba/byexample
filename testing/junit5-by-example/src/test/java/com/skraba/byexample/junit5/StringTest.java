d4732877fbd2e500931d2a4632c0435b14c151a4
Ryan Skraba <ryan@skraba.com>
2022-03-10 19:03:04 +0100

    feat(testing): Add CharSequence tests

--- testing/junit5-by-example/src/test/java/com/skraba/byexample/junit5/StringTest.java
+++ testing/junit5-by-example/src/test/java/com/skraba/byexample/junit5/StringTest.java
@@ -3,6 +3,7 @@ package com.skraba.byexample.junit5;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import java.nio.CharBuffer;
 import org.junit.jupiter.api.Test;
 
 /** Simple assertions on string primitives. */
@@ -17,5 +18,23 @@ class StringTest {
     assertThat(issue, startsWith("ABC"));
     assertThat(issue, endsWith("1234"));
     assertThat(issue, matchesRegex("[A-Z]+-\\d+"));
+  }
+
+  @Test
+  void testCharSequence() {
+    // Note that all CharSequence with the same contents aren't necessarily equal!
+    CharSequence issue = CharBuffer.wrap("ABC-1234");
+
+    // The CharBuffer is not equal to the "same" String
+    assertThat(issue, not("ABC-1234"));
+
+    // The workaround is to wrap matchers with hasToString(...)
+    assertThat(issue, hasToString("ABC-1234"));
+    assertThat(issue, hasToString(not(emptyOrNullString())));
+    assertThat(issue, hasToString(is("ABC-1234")));
+    assertThat(issue, hasToString(containsString("C-1")));
+    assertThat(issue, hasToString(startsWith("ABC")));
+    assertThat(issue, hasToString(endsWith("1234")));
+    assertThat(issue, hasToString(matchesRegex("[A-Z]+-\\d+")));
   }
 }
