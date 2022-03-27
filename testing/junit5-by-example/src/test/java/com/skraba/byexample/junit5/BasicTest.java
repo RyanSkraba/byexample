95a8272e2373fae43131f24d7fe7814e44d62770
Ryan Skraba <ryan@skraba.com>
2022-03-27 09:30:50 +0200

    test(junit5): Add testing an assertion

--- testing/junit5-by-example/src/test/java/com/skraba/byexample/junit5/BasicTest.java
+++ testing/junit5-by-example/src/test/java/com/skraba/byexample/junit5/BasicTest.java
@@ -1,6 +1,8 @@
 package com.skraba.byexample.junit5;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -25,5 +27,11 @@ class BasicTest {
     AssertionFailedError ex =
         assertThrows(AssertionFailedError.class, () -> fail("We arrived here"));
     assertThat(ex.getMessage(), is("We arrived here"));
+
+    // Assertions also throw exceptions
+    AssertionError ex2 =
+        assertThrows(AssertionError.class, () -> assertThat("X", is("Y")));
+    assertThat(ex2.getMessage(), containsString("Expected: is \"Y\""));
+    assertThat(ex2.getMessage(), containsString("but: was \"X\""));
   }
 }
