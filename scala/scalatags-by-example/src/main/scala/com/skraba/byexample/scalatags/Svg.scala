9c9ca5b943618a45213bb6f04c8efff191c81b5e
Ryan Skraba <ryan@skraba.com>
2022-08-18 08:11:30 +0200

    feat: Add license and fix alignment

--- scala/scalatags-by-example/src/main/scala/com/skraba/byexample/scalatags/Svg.scala
+++ scala/scalatags-by-example/src/main/scala/com/skraba/byexample/scalatags/Svg.scala
@@ -20,6 +20,26 @@ object Svg {
 
   val XmlNs = attr("xmlns") := "http://www.w3.org/2000/svg"
 
+  /** The Apache2 license header
+    * https://www.apache.org/licenses/LICENSE-2.0#apply
+    */
+  val Apache2: String =
+    s"""  Licensed to the Apache Software Foundation (ASF) under one or more
+       |  contributor license agreements.  See the NOTICE file distributed with
+       |  this work for additional information regarding copyright ownership.
+       |  The ASF licenses this file to You under the Apache License, Version 2.0
+       |  (the "License"); you may not use this file except in compliance with
+       |  the License.  You may obtain a copy of the License at
+       |
+       |      http://www.apache.org/licenses/LICENSE-2.0
+       |
+       |  Unless required by applicable law or agreed to in writing, software
+       |  distributed under the License is distributed on an "AS IS" BASIS,
+       |  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+       |  See the License for the specific language governing permissions and
+       |  limitations under the License.
+       |""".stripMargin
+
   /** Wraps the contents in an SVG tag. If the content is already an svg tag, it
     * isn't wrapped.
     *
@@ -163,7 +183,7 @@ object Svg {
     /** A tag vertically and right-aligned on the given rectangle. */
     def left(x: Double, y: Double, dx: Double, dy: Double): Tag = {
       base(
-        Attrs.x := 0,
+        Attrs.x := x,
         Attrs.y := y + dy / 2 + size * adjust,
         Attrs.textAnchor := "start",
         Attrs.dominantBaseline := "middle"
