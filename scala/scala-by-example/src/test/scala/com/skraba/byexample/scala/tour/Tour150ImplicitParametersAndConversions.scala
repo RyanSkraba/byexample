b8ae07089f9e419224eae61214b5c643bc525cf7
Ryan Skraba <ryan@skraba.com>
2022-10-29 11:28:36 +0200

    feat(scala): Show implicits coming from values

--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/tour/Tour150ImplicitParametersAndConversions.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/tour/Tour150ImplicitParametersAndConversions.scala
@@ -30,29 +30,60 @@ class Tour150ImplicitParametersAndConversions
         /** The implicit comparator can be a method */
         implicit def stringComparator: Comparator[String] =
           (x: String, y: String) => x.compareTo(y)
+
+        /** The implicit comparator can be a member */
+        implicit val sequenceComparator: Comparator[Seq[Int]] =
+          (x: Seq[Int], y: Seq[Int]) =>
+            IntComparator.compare(x.length, y.length)
       }
 
       def max[A](x: A, y: A)(implicit comparator: Comparator[A]): A =
         if (comparator.compare(x, y) >= 0) x
         else y
 
-      it("from companion objects") {
-        max(10, 6) shouldBe 10
-        max("hello", "world") shouldBe "world"
+      it("fail when they can't be found") {
         "max(true, false)" shouldNot compile
+      }
+
+      it("from a companion object object") {
+        max(10, 6) shouldBe 10
+      }
+
+      it("from a companion object method") {
+        max("hello", "world") shouldBe "world"
+      }
+
+      it("from a companion object value") {
+        max(Seq(-1, -100), Nil) shouldBe Seq(-1, -100)
       }
 
       it("from implicit objects in scope") {
         implicit object BooleanComparator extends Comparator[Boolean] {
           override def compare(x: Boolean, y: Boolean): Int = x.compareTo(y)
         }
-        implicit def ReverseIntComparator: Comparator[Int] =
-          (x: Int, y: Int) => y.compareTo(x)
-        max(10, 6) shouldBe 6
-        max("hello", "world") shouldBe "world" // still from companion
         max(true, false) shouldBe true
       }
 
+      it("from implicit methods in scope") {
+        implicit def ReverseIntComparator: Comparator[Int] =
+          (x: Int, y: Int) => y.compareTo(x)
+        max(10, 6) shouldBe 6
+      }
+
+      it("from implicit values in scope") {
+        // Only consider the head value
+        implicit val headComparator: Comparator[Seq[Int]] =
+          (x: Seq[Int], y: Seq[Int]) =>
+            x.zip(y)
+              .headOption
+              .map(Function.tupled(_.compareTo(_)))
+              .getOrElse(x.length.compareTo(y.length))
+
+        max(Seq(-1, -100), Nil) shouldBe Seq(-1, -100)
+        max(Seq(-1, -99), Seq(-1, 0)) shouldBe Seq(-1, -99)
+        max(Nil, Seq(-1, 0)) shouldBe Seq(-1, 0)
+        max(Seq(1), Seq(-1, 0)) shouldBe Seq(1)
+      }
     }
   }
 
