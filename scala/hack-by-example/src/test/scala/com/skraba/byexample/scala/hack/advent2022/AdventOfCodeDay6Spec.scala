09213b285a01c546a16dc44d0e4c15f93ddff110
Ryan Skraba <ryan@skraba.com>
2022-12-07 08:30:45 +0100

    feat(hack): Add Advent2022 day6

--- scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2022/AdventOfCodeDay6Spec.scala
+++ scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2022/AdventOfCodeDay6Spec.scala
@@ -5,13 +5,17 @@ import org.scalatest.BeforeAndAfterEach
 import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatest.matchers.should.Matchers
 
+import scala.util.matching.Regex
+
 /** =Advent of Code 2022 Day 6 Solutions in scala=
   *
-  * Input:
+  * Input: One string as a message with letters
   *
-  * Part 1:
+  * Part 1: The number of letters that you have to read before finding 4
+  * consecutive different letters
   *
-  * Part 2:
+  * Part 2: The number of letters that you have to read before finding 14
+  * consecutive different letters
   *
   * @see
   *   Rephrased from https://adventofcode.com/2022/day/6
@@ -22,34 +26,35 @@ class AdventOfCodeDay6Spec
     with BeforeAndAfterEach {
 
   object Solution {
+    def findStartOfPacket(in: String): Long =
+      in.sliding(4).indexWhere(_.toSet.size == 4) + 4
 
-    case class ABC(a: Long) {}
-
-    def parse(in: String): Option[ABC] = None
-
-    def part1(in: String*): Long = 100
-
-    def part2(in: String*): Long = 200
+    def findStartOfMessage(in: String): Long =
+      in.sliding(14).indexWhere(_.toSet.size == 14) + 14
   }
 
   import Solution._
 
   describe("Example case") {
-    val input =
-      """
-      |""".stripMargin.split("\n")
-
     it("should match the puzzle description") {
-      part1(input: _*) shouldBe 100
-      part2(input: _*) shouldBe 200
+      findStartOfPacket("mjqjpqmgbljsphdztnvjfqwrcgsmlb") shouldBe 7
+      findStartOfPacket("bvwbjplbgvbhsrlpgdmjqwftvncz") shouldBe 5
+      findStartOfPacket("nppdvjthqldpwncqszvftbrmjlhg") shouldBe 6
+      findStartOfPacket("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") shouldBe 10
+      findStartOfPacket("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") shouldBe 11
+      findStartOfMessage("mjqjpqmgbljsphdztnvjfqwrcgsmlb") shouldBe 19
+      findStartOfMessage("bvwbjplbgvbhsrlpgdmjqwftvncz") shouldBe 23
+      findStartOfMessage("nppdvjthqldpwncqszvftbrmjlhg") shouldBe 23
+      findStartOfMessage("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") shouldBe 29
+      findStartOfMessage("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") shouldBe 26
     }
   }
 
   describe("Solution") {
     val input = puzzleInput("Day6Input.txt")
     it("should have answers") {
-      part1(input: _*) shouldBe 100
-      part2(input: _*) shouldBe 200
+      findStartOfPacket(input.head) shouldBe 1582
+      findStartOfMessage(input.head) shouldBe 3588
     }
   }
 }
