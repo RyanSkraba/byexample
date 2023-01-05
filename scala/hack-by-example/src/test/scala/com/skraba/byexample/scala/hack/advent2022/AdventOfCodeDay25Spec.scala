69d22354c8865776dc08d79bdb3460cb18bf30e1
Ryan Skraba <ryan@skraba.com>
2023-01-05 08:40:20 +0100

    feat(hack): Add Advent2022 Day 25

--- scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2022/AdventOfCodeDay25Spec.scala
+++ scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2022/AdventOfCodeDay25Spec.scala
@@ -7,9 +7,9 @@ import org.scalatest.matchers.should.Matchers
 
 /** =Advent of Code 2022 Day 25 Solutions in scala=
   *
-  * Input:
+  * Input: A list of SNAFU formatted numbers.
   *
-  * Part 1:
+  * Part 1: The sum of the SNAFU numbers in decimal.
   *
   * Part 2:
   *
@@ -23,11 +23,32 @@ class AdventOfCodeDay25Spec
 
   object Solution {
 
-    case class ABC(a: Long) {}
+    def unSnafu(in: String): Long = in.foldLeft(0L) {
+      case (acc, '2') => acc * 5 + 2
+      case (acc, '1') => acc * 5 + 1
+      case (acc, '0') => acc * 5
+      case (acc, '-') => acc * 5 - 1
+      case (acc, '=') => acc * 5 - 2
+    }
 
-    def parse(in: String): Option[ABC] = None
+    def snafu(in: Long): String = Stream
+      .iterate(("", in)) {
+        case (acc, remainder) if remainder % 5 == 2 =>
+          ("2" + acc, remainder / 5)
+        case (acc, remainder) if remainder % 5 == 1 =>
+          ("1" + acc, remainder / 5)
+        case (acc, remainder) if remainder % 5 == 0 =>
+          ("0" + acc, remainder / 5)
+        case (acc, remainder) if remainder % 5 == 3 =>
+          ("=" + acc, remainder / 5 + 1)
+        case (acc, remainder) if remainder % 5 == 4 =>
+          ("-" + acc, remainder / 5 + 1)
+      }
+      .dropWhile(_._2 != 0)
+      .head
+      ._1
 
-    def part1(in: String*): Long = 4890
+    def part1(in: String*): String = snafu(in.map(unSnafu).sum)
 
     def part2(in: String*): Long = 200
   }
@@ -50,23 +71,27 @@ class AdventOfCodeDay25Spec
         |1=
         |122""".stripMargin.split("\n")
 
-    it("should match the puzzle description for part 1") {
-      part1(input: _*) shouldBe 4890
+    val inputDecimal =
+      Seq(1747, 906, 198, 11, 201, 31, 1257, 32, 353, 107, 7, 3, 37)
+
+    for (i <- input.indices) {
+      it(s"should unsnafu ${input(i)}") {
+        unSnafu(input(i)) shouldBe inputDecimal(i)
+      }
+      it(s"should snafu ${inputDecimal(i)}") {
+        snafu(inputDecimal(i)) shouldBe input(i)
+      }
     }
 
-    it("should match the puzzle description for part 2") {
-      part2(input: _*) shouldBe 200
+    it("should match the puzzle description for part 1") {
+      part1(input: _*) shouldBe "2=-1=0"
     }
   }
 
   describe("Solution") {
     val input = puzzleInput("Day25Input.txt")
     it("should have answers for part 1") {
-      part1(input: _*) shouldBe 4890
-    }
-
-    it("should have answers for part 2") {
-      part2(input: _*) shouldBe 200
+      part1(input: _*) shouldBe "2=--=0000-1-0-=1=0=2"
     }
   }
 }
