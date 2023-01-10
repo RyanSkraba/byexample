d5c1ef0b09d007dbd8b8a86399376aa03ff4f22a
Ryan Skraba <ryan@skraba.com>
2023-01-10 08:35:43 +0100

    refactor(scala): Beautify with spotless

--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/algorithms/GraphFloydWarshall.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/algorithms/GraphFloydWarshall.scala
@@ -5,9 +5,9 @@ import scala.collection.mutable
 class GraphFloydWarshall {
 
   def floydWarshall[T](
-                        vs: Iterable[T],
-                        edgeF: T => Iterator[T]
-                      ): Map[(T, T), Long] = {
+      vs: Iterable[T],
+      edgeF: T => Iterator[T]
+  ): Map[(T, T), Long] = {
     // Set up the distances between all vertices
     // If there's an initial edge, the distance is 1
     // The distance to itself is 0
@@ -19,17 +19,17 @@ class GraphFloydWarshall {
     for (mid <- vs; src <- vs; dst <- vs)
       if (
         dv(src -> mid) != Long.MaxValue &&
-          dv(mid -> dst) != Long.MaxValue &&
-          dv(src -> mid) + dv(mid -> dst) < dv(src -> dst)
+        dv(mid -> dst) != Long.MaxValue &&
+        dv(src -> mid) + dv(mid -> dst) < dv(src -> dst)
       ) dv += (src -> dst) -> (dv(src -> mid) + dv(mid -> dst))
 
     dv.toMap
   }
 
   def floydWarshallWithPath[T](
-                                vs: Iterable[T],
-                                edgeF: T => Iterator[T]
-                              ): Map[(T, T), (Long, Option[T])] = {
+      vs: Iterable[T],
+      edgeF: T => Iterator[T]
+  ): Map[(T, T), (Long, Option[T])] = {
     // Set up the distances between all vertices
     // If there's an initial edge, the distance is 1
     // The distance to itself is 0
@@ -44,8 +44,8 @@ class GraphFloydWarshall {
     for (mid <- vs; src <- vs; dst <- vs)
       if (
         dv(src -> mid)._1 != Long.MaxValue &&
-          dv(mid -> dst)._1 != Long.MaxValue &&
-          dv(src -> mid)._1 + dv(mid -> dst)._1 < dv(src -> dst)._1
+        dv(mid -> dst)._1 != Long.MaxValue &&
+        dv(src -> mid)._1 + dv(mid -> dst)._1 < dv(src -> dst)._1
       )
         dv += (src -> dst) -> (dv(src -> mid)._1 + dv(mid -> dst)._1, Some(
           mid
@@ -55,9 +55,9 @@ class GraphFloydWarshall {
   }
 
   def floydWarshallWithEdgeWeights[T](
-                                       vs: Iterable[T],
-                                       edgeFn: T => Iterator[(T, Long)]
-                                     ): Map[(T, T), Long] = {
+      vs: Iterable[T],
+      edgeFn: T => Iterator[(T, Long)]
+  ): Map[(T, T), Long] = {
     // Set up the distances between all vertices
     // The distance to itself is 0
     val dv = mutable.Map[(T, T), Long]().withDefaultValue(Long.MaxValue) ++
@@ -69,8 +69,8 @@ class GraphFloydWarshall {
     for (mid <- vs; src <- vs; dst <- vs)
       if (
         dv(src -> mid) != Long.MaxValue &&
-          dv(mid -> dst) != Long.MaxValue &&
-          dv(src -> mid) + dv(mid -> dst) < dv(src -> dst)
+        dv(mid -> dst) != Long.MaxValue &&
+        dv(src -> mid) + dv(mid -> dst) < dv(src -> dst)
       ) dv += (src -> dst) -> (dv(src -> mid) + dv(mid -> dst))
 
     dv.toMap
