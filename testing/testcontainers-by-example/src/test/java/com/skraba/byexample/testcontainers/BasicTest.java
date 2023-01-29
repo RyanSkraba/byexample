97e6b671d6a478fc87287021f22008195bb7ebe5
Ryan Skraba <ryan@skraba.com>
2023-01-29 04:44:13 +0100

    feat(testing): Add RestAssured to test containers

--- testing/testcontainers-by-example/src/test/java/com/skraba/byexample/testcontainers/BasicTest.java
+++ testing/testcontainers-by-example/src/test/java/com/skraba/byexample/testcontainers/BasicTest.java
@@ -1,14 +1,17 @@
 package com.skraba.byexample.testcontainers;
 
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
@@ -22,7 +25,7 @@ class BasicTest {
 
   @Container
   @SuppressWarnings("resource")
-  public GenericContainer<?> httpd =
+  private GenericContainer<?> httpd =
       new GenericContainer<>(DockerImageName.parse("httpd:2.4"))
           .withFileSystemBind(
               System.getProperty("user.dir") + "/src/test/resources/htdocs/",
@@ -30,20 +33,29 @@ class BasicTest {
               BindMode.READ_ONLY)
           .withExposedPorts(80);
 
-  @Test
-  public void testBasic() throws IOException, URISyntaxException, InterruptedException {
-    HttpClient client = HttpClient.newHttpClient();
-    String x = Paths.get(".").toAbsolutePath().toString();
+  private URI getHttdUri() throws IOException, URISyntaxException {
+    return new URL("http", httpd.getHost(), httpd.getFirstMappedPort(), "/").toURI();
+  }
 
+  @Test
+  void testBasic() throws IOException, URISyntaxException, InterruptedException {
+    HttpClient client = HttpClient.newHttpClient();
     HttpRequest request =
-        HttpRequest.newBuilder()
-            .uri(new URL("http", httpd.getHost(), httpd.getFirstMappedPort(), "/").toURI())
-            .header("Accept", "text/html")
-            .GET()
-            .build();
+        HttpRequest.newBuilder().uri(getHttdUri()).header("Accept", "text/html").GET().build();
 
     HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
     assertThat(httpResponse.statusCode()).isEqualTo(200);
     assertThat(httpResponse.body()).isEqualTo("<html><body><h1>It works!</h1></body></html>\n");
+  }
+
+  @Test
+  public void testJson() throws IOException, URISyntaxException {
+    given()
+        .baseUri(getHttdUri().toString())
+        .when()
+        .get("/lotto/{id}.json", 5)
+        .then()
+        .statusCode(200)
+        .body("lotto.lottoId", equalTo(5), "lotto.winners.winnerId", hasItems(23, 54));
   }
 }
