cf40675b413afa8e2584830439a70fa427b79a4f
Ryan Skraba <ryan@skraba.com>
2023-02-01 03:30:06 +0100

    feat: Only launch one httpd in test

--- testing/testcontainers-by-example/src/test/java/com/skraba/byexample/testcontainers/BasicTest.java
+++ testing/testcontainers-by-example/src/test/java/com/skraba/byexample/testcontainers/BasicTest.java
@@ -1,10 +1,11 @@
 package com.skraba.byexample.testcontainers;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
 
+import io.restassured.RestAssured;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -13,6 +14,7 @@ import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
@@ -26,7 +28,7 @@ class BasicTest {
 
   @Container
   @SuppressWarnings("resource")
-  private GenericContainer<?> httpd =
+  private static GenericContainer<?> httpd =
       new GenericContainer<>(DockerImageName.parse("httpd:2.4"))
           .withFileSystemBind(
               System.getProperty("user.dir") + "/src/test/resources/htdocs/",
@@ -34,7 +36,12 @@ class BasicTest {
               BindMode.READ_ONLY)
           .withExposedPorts(80);
 
-  private URI getHttdUri() throws RuntimeException {
+  @BeforeAll
+  static void setup() {
+    RestAssured.baseURI = getHttpdUri().toString();
+  }
+
+  private static URI getHttpdUri() throws RuntimeException {
     try {
       return new URL("http", httpd.getHost(), httpd.getFirstMappedPort(), "/").toURI();
     } catch (URISyntaxException | MalformedURLException e) {
@@ -46,7 +53,7 @@ class BasicTest {
   void testBasic() throws IOException, InterruptedException {
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request =
-        HttpRequest.newBuilder().uri(getHttdUri()).header("Accept", "text/html").GET().build();
+        HttpRequest.newBuilder().uri(getHttpdUri()).header("Accept", "text/html").GET().build();
 
     HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
     assertThat(httpResponse.statusCode()).isEqualTo(200);
@@ -55,9 +62,7 @@ class BasicTest {
 
   @Test
   public void testJson() {
-    given()
-        .baseUri(getHttdUri().toString())
-        .when()
+    when()
         .get("/lotto/{id}.json", 5)
         .then()
         .statusCode(200)
