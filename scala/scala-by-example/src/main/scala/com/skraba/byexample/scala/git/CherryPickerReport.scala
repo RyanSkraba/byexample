17211dd391ded65671a32a3f2cb96b7d7f7ea484
Ryan Skraba <ryan@skraba.com>
2023-05-13 11:09:27 +0200

    feat: Add issues links to report

--- scala/scala-by-example/src/main/scala/com/skraba/byexample/scala/git/CherryPickerReport.scala
+++ scala/scala-by-example/src/main/scala/com/skraba/byexample/scala/git/CherryPickerReport.scala
@@ -19,6 +19,7 @@ case class CherryPickerReport(
     rBranch: String,
     left: Seq[Commit],
     right: Seq[Commit],
+    issuesUrl: Option[String] = None,
     doc: Header = Header(0, ""),
     cfg: ColourCfg = AnsiColourCfg
 ) {
@@ -32,6 +33,10 @@ case class CherryPickerReport(
   lazy val leftSubjects: Map[String, Seq[Commit]] = left.groupBy(_.subject)
   lazy val rightSubjects: Map[String, Seq[Commit]] = right.groupBy(_.subject)
 
+  lazy val IssuesTag: Option[String] = issuesUrl.map {
+    _.reverse.dropWhile(_ == '/').takeWhile(_.isUpper).reverse
+  }
+
   /** Rerun this report using the updated commits. */
   def update(current: CherryPickerReport): CherryPickerReport =
     copy(left = current.left, right = current.right)
@@ -43,15 +48,6 @@ case class CherryPickerReport(
     if (cmt.isBump && !row(2).toLowerCase.contains("bump"))
       row.updated(2, "Bump " + row(2))
     else row
-
-  private[this] def rowUpdateBug(cmt: Commit)(row: TableRow): TableRow = {
-    val bugs =
-      for (patternMatch <- IssueRegex.findAllMatchIn(cmt.subject))
-        yield patternMatch.group(1)
-    bugs.foldLeft(row)((r, bug) =>
-      if (r(2).contains(s"[$bug]")) r else r.updated(2, s"[$bug] " + r(2))
-    )
-  }
 
   private[this] def rowUpdatePossibleCherryPick(cmt: Commit)(
       otherSubjects: Map[String, Seq[Commit]]
@@ -120,7 +116,6 @@ case class CherryPickerReport(
             Some(r)
               .map(rowUpdateBump(commit))
               .map(rowUpdatePossibleCherryPick(commit)(otherSubjects))
-              // .map(rowUpdateBug(commit))
               .map(rowTrim)
               .get
         })
@@ -128,17 +123,18 @@ case class CherryPickerReport(
   }
 
   def toDoc: Header = {
-    doc
+    // Generate the entire document
+    val preIssues = doc
       .mapFirstIn(ifNotFound = Header(1, "CherryPickerReport")) {
         case h1: Header if h1.title == "CherryPickerReport" =>
           h1.mapFirstIn(ifNotFound =
             // TODO Table.from without alignments
             Seq(
               Table
-                .from(Seq.fill(2)(Align.LEFT), TableRow.from("Branch info", ""))
+                .from(Seq.fill(2)(Align.LEFT), TableRow.from(ConfigTable, ""))
             )
           ) {
-            case tbl: Table if tbl.title.startsWith("Branch info") =>
+            case tbl: Table if tbl.title.startsWith(ConfigTable) =>
               tbl
                 .mapFirstIn(ifNotFound = TableRow.from("Left", "")) {
                   case row if row.head == "Left" =>
@@ -185,7 +181,7 @@ case class CherryPickerReport(
           addTableOfCommits(h1, right.reverse, leftSubjects)
       }
       .mapFirstIn(ifNotFound = Header(1, "References")) {
-        case h1: Header if h1.title == "References" =>
+        case h1: Header if h1.level == 1 && h1.title == "References" =>
           val linkRefs: Seq[LinkRef] = (left ++ right).map(cmt =>
             LinkRef(
               cmt.commit.take(CommitHashSize),
@@ -195,6 +191,43 @@ case class CherryPickerReport(
           )
           h1.copy(mds = h1.mds ++ linkRefs)
       }
+
+    // If there is an issue tag, then reprocess the document entirely, linking all of the issues to the site
+    IssuesTag
+      .map { tag =>
+        // Get the entire document as text
+        val preTxt: String = preIssues.build().toString
+
+        // Replaces all issue tags that aren't already surrounded by square brackets with ones that are
+        val post = Header.parse(
+          s"(?<!\\[)\\b$tag-\\d+(?<!])".r.replaceAllIn(preTxt, "[$0]")
+        )
+
+        // Find all of the issues numbers that are referred to in the doc.
+        val issueRegex = s"\\b$tag-(\\d+)\\b".r
+        val allIssueNum =
+          (for (patternMatch <- issueRegex.findAllMatchIn(preTxt))
+            yield patternMatch.group(1)).toSet
+
+        // All of the original references in that section.
+        val originalReferences = preIssues
+          .collectFirstRecursive {
+            case h1: Header if h1.level == 1 && h1.title == "References" => h1
+          }
+          .getOrElse(Header(1, "References"))
+
+        // Add any missing link references
+        post.mapFirstIn() {
+          case h1: Header if h1.level == 1 && h1.title == "References" =>
+            allIssueNum.foldLeft(originalReferences) { (acc, num) =>
+              val tagToCheck = s"$tag-$num"
+              acc.mapFirstIn(ifNotFound =
+                LinkRef(tagToCheck, s"${issuesUrl.get}-$num")
+              ) { case lr @ LinkRef(tagToCheck, _, _) => lr }
+            }
+        }
+      }
+      .getOrElse(preIssues)
   }
 
   private[this] def summarizeSubjects(
@@ -263,8 +296,9 @@ object CherryPickerReport {
 
   val MinCommitHashSize = 7
 
-  val IssueRegex: Regex = raw"(AVRO-(\d+))".r
+  val ConfigTable = "Project Info"
 
+  /** @return True if the text contains a reference to the given commit hash. */
   private def refersTo(hash: String, text: String): Boolean = {
     for (i <- MinCommitHashSize to hash.length)
       if (text.contains(s"[${hash.take(i)}]")) return true;
@@ -308,16 +342,24 @@ object CherryPickerReport {
     */
   def fromDoc(doc: Header): CherryPickerReport = {
 
-    val (lBranch, rBranch) = doc
+    val (lBranch, rBranch, issues: Option[String]) = doc
       .collectFirstRecursive {
-        case tbl @ Table(_, rows) if tbl.title.startsWith("Branch info") =>
+        case tbl: Table if tbl.title.startsWith(ConfigTable) =>
           (
             tbl.mds.find(_.head == "Left").map(_.cells(1)).getOrElse(""),
-            tbl.mds.find(_.head == "Right").map(_.cells(1)).getOrElse("")
+            tbl.mds.find(_.head == "Right").map(_.cells(1)).getOrElse(""),
+            tbl.mds.find(_.head == "Issues").map(_.cells(1))
           )
       }
-      .getOrElse(("", ""))
+      .getOrElse(("", "", None))
 
-    CherryPickerReport(lBranch, rBranch, Seq.empty, Seq.empty, doc = doc)
+    CherryPickerReport(
+      lBranch,
+      rBranch,
+      Seq.empty,
+      Seq.empty,
+      issuesUrl = issues,
+      doc = doc
+    )
   }
 }
--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/git/CherryPickerReportSpec.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/git/CherryPickerReportSpec.scala
@@ -1,8 +1,12 @@
 package com.skraba.byexample.scala.git
 
 import com.skraba.byexample.scala.git.CherryPickerReport
+import com.skraba.byexample.scala.markd.Header
 import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatest.matchers.should.Matchers
+import os.CommandResult
+
+import scala.util.Try
 
 /** Unit tests for [[CherryPickerReport]]
   */
@@ -10,11 +14,23 @@ class CherryPickerReportSpec extends AnyFunSpecLike with Matchers {
 
   describe("Test") {
     ignore("works") {
-      CherryPickerReport.fromGit(
-        (os.home / "working" / "apache" / "avro").toString,
-        "master",
-        "branch-1.11"
+      val x: Either[CherryPickerReport, Try[CommandResult]] =
+        CherryPickerReport.fromGit(
+          (os.home / "working" / "apache" / "avro").toString,
+          "master",
+          "branch-1.11"
+        )
+
+      val y = CherryPickerReport.fromDoc(
+        Header.parse(
+          """| Project Info |                                            |
+           |--------------|--------------------------------------------|
+           | Issues       | https://issues.apache.org/jira/browse/AVRO |
+           |""".stripMargin
+        )
       )
+
+      x.swap.map(y.update).map(_.toDoc).foreach(z => println(z))
     }
   }
 }
