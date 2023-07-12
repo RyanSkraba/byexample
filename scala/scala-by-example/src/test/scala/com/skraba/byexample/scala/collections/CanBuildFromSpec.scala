c1b4a94a37cedb53b7588b8f4030cab91078fb36
Ryan Skraba <ryan@skraba.com>
2023-07-12 08:06:07 +0200

    feat: Add BuildFrom example for 2.13

--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/collections/CanBuildFromSpec.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/collections/CanBuildFromSpec.scala
@@ -3,38 +3,37 @@ package com.skraba.byexample.scala.collections
 import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatest.matchers.should.Matchers
 
-// TODO: This is deprecated and this class requires a rewrite
-// import scala.collection.generic.CanBuildFrom
-import scala.collection.mutable
+import scala.collection.BuildFrom
 
-/** Collections and [[CanBuildFrom]]
+/** Collections and [[BuildFrom]]
   *
   * @see
   *   [[https://www.scala-lang.org/blog/2017/05/30/tribulations-canbuildfrom.html]]
   */
 class CanBuildFromSpec extends AnyFunSpecLike with Matchers {
-// TODO
-//  describe("CanBuildFrom") {
-//
-//    it("is used to ensure that a String can be mapped to a String") {
-//
-//      // A string is a implicitly sequence of characters.
-//      val orig = "Hello world"
-//      orig shouldBe a[String]
-//      "val check: Seq[Char] = orig" should compile
-//
-//      // So you can map it to a sequence of integers.
-//      "val check: Seq[Int] = orig.map(_.toInt)" should compile
-//
-//      // Or magically to a String (or sequence of characters, whatever).
-//      "val check: Seq[Char] = orig.map(_.toUpper)" should compile
-//      "val check: String = orig.map(_.toUpper)" should compile
-//
-//      // This magic comes from the Predefs StringCanBuildFrom!
-//      val scbf: CanBuildFrom[String, Char, String] = implicitly
-//      scbf shouldBe StringCanBuildFrom
-//    }
-//  }
+
+  describe("BuildFrom") {
+
+    it("is used to ensure that a String can be mapped to a String") {
+
+      // A string is a implicitly sequence of characters.
+      val orig = "Hello world"
+      orig shouldBe a[String]
+      "val check: Seq[Char] = orig" should compile
+
+      // So you can map it to a sequence of integers.
+      "val check: Seq[Int] = orig.map(_.toInt)" should compile
+
+      // Or magically to a String (or sequence of characters, whatever).
+      "val check: Seq[Char] = orig.map(_.toUpper)" should compile
+      "val check: String = orig.map(_.toUpper)" should compile
+
+      // This magic comes from the Predefs StringCanBuildFrom!
+      val scbf: BuildFrom[String, Char, String] = implicitly
+      scbf shouldBe BuildFrom.buildFromString
+    }
+  }
+
 //
 //  /** A custom list implementation that I want to behave the same way.
 //    *
