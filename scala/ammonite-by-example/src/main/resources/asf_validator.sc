7ba0e85a30b9ca6f623f82cc189720f092315e71
Ryan Skraba <ryan@skraba.com>
2023-08-19 11:51:13 +0200

    feat: Set and test ASF validation parameters from a file.

--- scala/ammonite-by-example/src/main/resources/asf_validator.sc
+++ scala/ammonite-by-example/src/main/resources/asf_validator.sc
@@ -3,6 +3,9 @@
 /** Validating a release for the ASF. */
 import mainargs.{ParserForClass, arg, main}
 
+import java.util.Properties
+import scala.jdk.CollectionConverters.PropertiesHasAsScala
+
 // ==========================================================================
 // Adding artifacts to your local build (from this project, from maven and
 // from local maven).
@@ -16,16 +19,41 @@ import com.skraba.byexample.scala.ammonite.ColourCfg
 // ==========================================================================
 // Top level variables available to the script
 
+/**
+ * Configuration parameters that can be used for this tool.
+ *
+ * @param cfgFile A file to read all of the parameters from
+ * @param key Override the key (top-level project name)
+ * @param svnUrl The SVN url that contains the *dev* artifacts for the toplevel project
+ * @param svnDir The local directory where SVN is checked out
+ */
 case class AsfReleaseConfig(
     cfgFile: Option[os.Path] = None,
-    key: String = "flink",
-    svn: String = "https://dist.apache.org/repos/dist/dev/flink/",
-    svnDir: os.Path = os.home/ "working"/"apache"/"asf-svn"/"flink-dev-dist"
+    key: Option[String] =None,
+    svnUrl: Option[String] =None,
+    svnDir: Option[os.Path] =None
+
 ) {
-  lazy val Key: String = key
-  lazy val Svn: String = svn
-  lazy val SvnDir: os.Path = svnDir
+
+  import AsfReleaseConfig._
+
+  val baseCfg : Map[String, String] = cfgFile.map{ cfg=>
+    val tmp : Properties = new Properties()
+    tmp.load(cfg.getInputStream)
+    tmp
+   }.getOrElse(new Properties()).asScala.toMap
+
+  lazy val Key: String = key.orElse(baseCfg.get("key")).getOrElse(DefaultKey)
+  lazy val SvnUrl: String = svnUrl.orElse(baseCfg.get("svnUrl")).getOrElse(DefaultSvnUrl)
+  lazy val SvnDir: os.Path = svnDir.orElse(baseCfg.get("svnDir").map(os.Path(_))).getOrElse(DefaultSvnDir)
 }
+
+object AsfReleaseConfig {
+ val DefaultKey: String = "flink";
+  val DefaultSvnUrl: String  = "https://dist.apache.org/repos/dist/dev/flink/"
+  val DefaultSvnDir: os.Path  = os.home / "working" / "apache" / "asf-svn" / "flink-dev-dist"
+}
+
 implicit def asfReleaseConfigParser: ParserForClass[AsfReleaseConfig] =
   ParserForClass[AsfReleaseConfig]
 
@@ -52,9 +80,9 @@ def help(asf: AsfReleaseConfig, cfg: ColourCfg): Unit = {
   println(
     s"""${cfg.bold("Environment:")}
        |
-       | ${cfg.left("key")} ${cfg.right(asf.Key)}
-       | ${cfg.left("svn")} ${cfg.right(asf.Svn)}
-       | ${cfg.left("svnDir")} ${cfg.right(asf.SvnDir)}
+       |${cfg.left("key")}=${cfg.right(asf.Key)}
+       |${cfg.left("svnUrl")}=${cfg.right(asf.SvnUrl)}
+       |${cfg.left("svnDir")}=${cfg.right(asf.SvnDir)}
        |
        |""".stripMargin
   )
--- scala/ammonite-by-example/src/test/scala/com/skraba/byexample/scala/ammonite/AsfValidatorSpec.scala
+++ scala/ammonite-by-example/src/test/scala/com/skraba/byexample/scala/ammonite/AsfValidatorSpec.scala
@@ -1,7 +1,11 @@
 package com.skraba.byexample.scala.ammonite
 
+import java.io.StringReader
+import java.util.Properties
 import scala.Console._
+import scala.collection.mutable
 import scala.io.AnsiColor.{BOLD, RESET}
+import scala.jdk.CollectionConverters.PropertiesHasAsScala
 import scala.reflect.io.File
 
 /** Test the file_renamer.sc script. */
@@ -29,6 +33,23 @@ class AsfValidatorSpec extends AmmoniteScriptSpecBase {
       }
     }
 
+    /** Helper to extract the environment from the given help text.
+      * @param args
+      *   The arguments to use in the script, in addition to "help" and
+      *   "--plain" (to avoid ANSI colouring).
+      * @return
+      *   A map containing the environment for the script as received from the
+      *   arguments, potentially a config file, and the defaults.
+      */
+    def extractEnvFromHelp(args: String*): mutable.Map[String, String] = {
+      val txtHelp =
+        help((Seq("--plain") ++ args): _*).split("Environment:\n\n", 2)
+      txtHelp should have size 2
+      val asProps = new Properties()
+      asProps.load(new StringReader(txtHelp(1)))
+      asProps.asScala
+    }
+
     it("should print a useful message") {
       // with helpers
       val ansiHelp = help()
@@ -40,5 +61,42 @@ class AsfValidatorSpec extends AmmoniteScriptSpecBase {
         "asf_validator.sc - Validating a release for the ASF"
       )
     }
+
+    it("should print the environment with flink defaults") {
+      val env = extractEnvFromHelp()
+      env("key") shouldBe "flink"
+      env("svnDir") should endWith("working/apache/asf-svn/flink-dev-dist")
+      env("svnUrl") shouldBe "https://dist.apache.org/repos/dist/dev/flink/"
+    }
+
+    it(
+      "should print the environment with custom values from the command line"
+    ) {
+      val env = extractEnvFromHelp("--key", "avro")
+      env("key") shouldBe "avro"
+      env("svnDir") should endWith("working/apache/asf-svn/flink-dev-dist")
+      env("svnUrl") shouldBe "https://dist.apache.org/repos/dist/dev/flink/"
+    }
+
+    it("should print the environment with custom values from a config file") {
+      val cfgFile = Tmp / "config.properties"
+      cfgFile.toFile.writeAll(
+        "key=iceberg\n",
+        "svnUrl=https://dist.apache.org/repos/dist/dev/iceberg/\n"
+      )
+
+      val env = extractEnvFromHelp("--cfgFile", cfgFile.toString)
+      env("key") shouldBe "iceberg"
+      env("svnDir") should endWith("working/apache/asf-svn/flink-dev-dist")
+      env("svnUrl") shouldBe "https://dist.apache.org/repos/dist/dev/iceberg/"
+
+      // The key from the command line is prioritized, then the file, then the default
+      val env2 =
+        extractEnvFromHelp("--cfgFile", cfgFile.toString, "--key", "pekko")
+      env2("key") shouldBe "pekko"
+      env2("svnDir") should endWith("working/apache/asf-svn/flink-dev-dist")
+      env2("svnUrl") shouldBe "https://dist.apache.org/repos/dist/dev/iceberg/"
+    }
+
   }
 }
