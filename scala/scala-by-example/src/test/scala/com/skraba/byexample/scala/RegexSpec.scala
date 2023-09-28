3c41cc598024996254a3e7e4c0a841140756d732
Ryan Skraba <ryan@skraba.com>
2023-09-28 09:18:04 +0200

    feat(scala): Add regex methods

--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/RegexSpec.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/RegexSpec.scala
@@ -15,6 +15,7 @@ class RegexSpec extends AnyFunSpecLike with Matchers {
   /** Group into the project and the issue number, like BYEX-1234 */
   val IssueRegex: Regex = raw"([A-Z]+)-(\d+)".r
 
+  /** Split on :: or , or ; punctuation. */
   val SplitRegex: Regex = raw"(::|,|;)".r
 
   // Constructing regular expressions
@@ -34,7 +35,22 @@ class RegexSpec extends AnyFunSpecLike with Matchers {
       IssueRegex.findFirstIn("No match") shouldBe None
       IssueRegex.findFirstIn("BYEX-1234") shouldBe Some("BYEX-1234")
       IssueRegex.findFirstIn("This is BYEX-123.") shouldBe Some("BYEX-123")
+      IssueRegex.findFirstIn("Not BYEX-123 but BYEX-234.") shouldBe Some(
+        "BYEX-123"
+      )
       IssueRegex.findFirstIn("byex-23 BYEX -123") shouldBe None
+    }
+
+    it("should find all matches anywhere in the string") {
+      // findAllIn returns Iterator[String]
+      IssueRegex.findAllIn("No match") shouldBe empty
+      IssueRegex.findAllIn("BYEX-1234").toSeq shouldBe Seq("BYEX-1234")
+      IssueRegex.findAllIn("This is BYEX-123.").toSeq shouldBe Seq("BYEX-123")
+      IssueRegex.findAllIn("Not BYEX-123 but BYEX-234.").toSeq shouldBe Seq(
+        "BYEX-123",
+        "BYEX-234"
+      )
+      IssueRegex.findAllIn("byex-23 BYEX -123") shouldBe empty
     }
 
     it("should split based on a regex") {
