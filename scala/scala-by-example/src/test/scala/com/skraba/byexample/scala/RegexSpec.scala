a428972ecbe6050d219574c15c680dd3d4b288e9
Ryan Skraba <ryan@skraba.com>
2023-09-29 09:13:11 +0200

    feat: Add Regex.Match examples

--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/RegexSpec.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/RegexSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatest.matchers.should.Matchers
 
 import scala.util.matching.Regex
+import org.scalatest.OptionValues._
 
 /** Using [[Regex]] in Scala.
   *
@@ -13,7 +14,7 @@ import scala.util.matching.Regex
 class RegexSpec extends AnyFunSpecLike with Matchers {
 
   /** Group into the project and the issue number, like BYEX-1234 */
-  val IssueRegex: Regex = raw"([A-Z]+)-(\d+)".r
+  val IssueRegex: Regex = raw"(?<prj>[A-Z]+)-(?<num>\d+)".r
 
   /** Split on :: or , or ; punctuation. */
   val SplitRegex: Regex = raw"(::|,|;)".r
@@ -21,10 +22,7 @@ class RegexSpec extends AnyFunSpecLike with Matchers {
   // Constructing regular expressions
 
   describe("Using Regex") {
-    // findAllIn
-    // findAllMatchIn
-    // findFirstIn
-    // findFirstMatchIn
+
     // findPrefixOf
     // findPrefixMatchOf
     // replaceAllIn (x2)
@@ -32,6 +30,7 @@ class RegexSpec extends AnyFunSpecLike with Matchers {
     // replaceFirstIn
 
     it("should find the first match anywhere in the string") {
+      // findFirstIn returns Option[String]
       IssueRegex.findFirstIn("No match") shouldBe None
       IssueRegex.findFirstIn("BYEX-1234") shouldBe Some("BYEX-1234")
       IssueRegex.findFirstIn("This is BYEX-123.") shouldBe Some("BYEX-123")
@@ -39,6 +38,30 @@ class RegexSpec extends AnyFunSpecLike with Matchers {
         "BYEX-123"
       )
       IssueRegex.findFirstIn("byex-23 BYEX -123") shouldBe None
+    }
+
+    it("should find information in the match") {
+      // the findFirstMatch returns the instance describing the match, not the matching string
+      val m = IssueRegex.findFirstMatchIn("Not BYEX-123 but BYEX-234.")
+      m.value.start shouldBe 4
+      m.value.end shouldBe 12
+      m.value.source shouldBe "Not BYEX-123 but BYEX-234."
+      m.value.matched shouldBe "BYEX-123"
+      m.value.groupCount shouldBe 2
+      m.value.group(0) shouldBe "BYEX-123"
+      m.value.group(1) shouldBe "BYEX"
+      m.value.group(2) shouldBe "123"
+      m.value.subgroups shouldBe List("BYEX", "123")
+      m.value.group("prj") shouldBe "BYEX"
+      m.value.group("num") shouldBe "123"
+      m.value.before shouldBe "Not "
+      m.value.before(0) shouldBe "Not "
+      m.value.before(1) shouldBe "Not "
+      m.value.before(2) shouldBe "Not BYEX-"
+      m.value.after shouldBe " but BYEX-234."
+      m.value.after(0) shouldBe " but BYEX-234."
+      m.value.after(1) shouldBe "-123 but BYEX-234."
+      m.value.after(2) shouldBe " but BYEX-234."
     }
 
     it("should find all matches anywhere in the string") {
@@ -54,6 +77,7 @@ class RegexSpec extends AnyFunSpecLike with Matchers {
     }
 
     it("should split based on a regex") {
+      // Endings tokens are thrown away
       SplitRegex.split("a::b,c") shouldBe Seq("a", "b", "c")
       SplitRegex.split("a:::b::c") shouldBe Seq("a", ":b", "c")
       SplitRegex.split("a::::b::c") shouldBe Seq("a", "", "b", "c")
@@ -63,7 +87,6 @@ class RegexSpec extends AnyFunSpecLike with Matchers {
   }
 
   describe("Using Regex extractors in a match") {
-
     it("should extract groups to variables") {
       ("BYEX-1234" match {
         case IssueRegex(prj, num) => Some((prj, num))
