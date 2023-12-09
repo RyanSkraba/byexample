e00d9e88e468a63a54451181b8e5d73bca76a68a
Ryan Skraba <ryan@skraba.com>
2023-12-09 18:33:42 +0100

    feat: Add Advent of Code day 9

--- scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2023/AdventOfCodeDay9Spec.scala
+++ scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2023/AdventOfCodeDay9Spec.scala
@@ -7,11 +7,13 @@ import org.scalatest.matchers.should.Matchers
 
 /** =Advent of Code 2023 Day 9 Solutions in scala=
   *
-  * Input:
+  * Input: A list of number sequences, one line per sequence. The delta between
+  * each number is a constant *OR* the delta between each delta is a constant or
+  * the delta between _those_ deltas are constants or...
   *
-  * Part 1:
+  * Part 1: Find the next number in each sequence, and sum them together.
   *
-  * Part 2:
+  * Part 2: Find the previous number in each sequence, and sum them together.
   *
   * @see
   *   Rephrased from [[https://adventofcode.com/2023/day/9]]
@@ -22,36 +24,50 @@ class AdventOfCodeDay9Spec
     with BeforeAndAfterEach {
 
   object Solution {
+    def part1extrapolate(in: Long*): Long = {
+      val y = LazyList.iterate(in) {
+        _.sliding(2).toSeq.map { xs => xs(1) - xs.head }
+      }
+      val nn = y.takeWhile(_.exists(_ != 0))
+      nn.map { _.last }.sum
+    }
 
-    case class ABC(a: Long) {}
+    def part1(in: String*): Long = {
+      in.map(_.split("\\s+").map(_.toLong)).map(part1extrapolate(_: _*)).sum
+    }
 
-    def parse(in: String): Option[ABC] = None
-
-    def part1(in: String*): Long = 100
-
-    def part2(in: String*): Long = 200
+    def part2(in: String*): Long = {
+      in.map(_.split("\\s+").reverse.map(_.toLong))
+        .map(part1extrapolate(_: _*))
+        .sum
+    }
   }
 
   import Solution._
 
   describe("Example case") {
     val input =
-      """
+      """0 3 6 9 12 15
+        |1 3 6 10 15 21
+        |10 13 16 21 30 45
         |""".trim.stripMargin.split("\n")
 
     it("should match the puzzle description for part 1") {
-      part1(input: _*) shouldBe 100
+      part1extrapolate(1, 1, 1) shouldBe 1
+      part1extrapolate(1, 2, 3, 4, 5) shouldBe 6
+      part1(input: _*) shouldBe 114
     }
 
     it("should match the puzzle description for part 2") {
-      part2(input: _*) shouldBe 200
+      part2(input: _*) shouldBe 2
     }
   }
 
   describe("🔑 Solution 🔑") {
     lazy val input = puzzleInput("Day9Input.txt")
-    lazy val answer1 = decryptLong("tTNGygZ0+O4PEH+5IiCrBw==")
-    lazy val answer2 = decryptLong("U9BZNCixKWAgOXNrGyDe5A==")
+    lazy val answer1 = decryptLong("/3DZIAsPsnsRIjv9NxbfHg==")
+    lazy val answer2 = decryptLong("sPtmgR95P6JS2265Rc863A==")
+
     it("should have answers for part 1") {
       part1(input: _*) shouldBe answer1
     }
