67cb7456946106f169101caa1ba440dccc720f25
Ryan Skraba <ryan@skraba.com>
2023-12-11 19:00:55 +0100

    feat: Add Advent of Code day 11

--- scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2023/AdventOfCodeDay11Spec.scala
+++ scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2023/AdventOfCodeDay11Spec.scala
@@ -7,11 +7,15 @@ import org.scalatest.matchers.should.Matchers
 
 /** =Advent of Code 2023 Day 11 Solutions in scala=
   *
-  * Input:
+  * Input: A map of galaxies in the sky.
   *
-  * Part 1:
+  * Part 1: Given a rate of expansion where all of the empty rows and columns
+  * have doubled in size, find the sum of the taxi distances between each galaxy
+  * pair.
   *
-  * Part 2:
+  * Part 2: Given a rate of expansion where all of the empty rows and columns
+  * are a million times in size, find the sum of the taxi distances between each
+  * galaxy pair.
   *
   * @see
   *   Rephrased from [[https://adventofcode.com/2023/day/11]]
@@ -23,41 +27,85 @@ class AdventOfCodeDay11Spec
 
   object Solution {
 
-    case class ABC(a: Long) {}
+    def part1(in: String*): Long = part2(2, in: _*)
 
-    def parse(in: String): Option[ABC] = None
+    def part2(age: Long, in: String*): Long = {
+      // The positions of all the galaxies
+      val glx =
+        for (y <- in.indices; x <- in(y).indices if in(y)(x) == '#')
+          yield (x, y)
 
-    def part1(in: String*): Long = 100
+      // Find all of the rows and columns that have galaxies
+      val xs = glx.map(_._1).toSet
+      val ys = glx.map(_._2).toSet
 
-    def part2(in: String*): Long = 200
+      // For each column, find if it has no galaxies, and transform that into
+      // the count of empty columns to its left (expanded to the age of the
+      // universe).  Same with rows.
+      val noXs = (0 to xs.max)
+        .map(xs(_))
+        .scanLeft(0L) { (total, exists) =>
+          if (exists) total else total + age - 1
+        }
+        .drop(1)
+      val noYs = (0 to ys.max)
+        .map(ys(_))
+        .scanLeft(0L) { (total, exists) =>
+          if (exists) total else total + age - 1
+        }
+        .drop(1)
+
+      // We can then calculate the manhattan distance between any two galaxies by
+      // adding the empty rows, columns.
+      val distances =
+        for (i1 <- glx.indices; i2 <- i1 + 1 until glx.size) yield {
+          val ((x1, y1), (x2, y2)) = (glx(i1), glx(i2))
+          math.abs(noXs(x1) + x1 - noXs(x2) - x2) + math.abs(
+            noYs(y1) + y1 - noYs(y2) - y2
+          )
+        }
+
+      distances.sum
+    }
   }
 
   import Solution._
 
   describe("Example case") {
     val input =
-      """
+      """...#......
+        |.......#..
+        |#.........
+        |..........
+        |......#...
+        |.#........
+        |.........#
+        |..........
+        |.......#..
+        |#...#.....
         |""".trim.stripMargin.split("\n")
 
     it("should match the puzzle description for part 1") {
-      part1(input: _*) shouldBe 100
+      part1(input: _*) shouldBe 374
     }
 
     it("should match the puzzle description for part 2") {
-      part2(input: _*) shouldBe 200
+      part2(10, input: _*) shouldBe 1030
+      part2(100, input: _*) shouldBe 8410
     }
   }
 
   describe("🔑 Solution 🔑") {
-    lazy val input = puzzleInput("Day11Input.txt")
-    lazy val answer1 = decryptLong("tTNGygZ0+O4PEH+5IiCrBw==")
-    lazy val answer2 = decryptLong("U9BZNCixKWAgOXNrGyDe5A==")
+    val input = puzzleInput("Day11Input.txt")
+    lazy val answer1 = decryptLong("LBMs8fFs30lQX2fhFWXtGw==")
+    lazy val answer2 = decryptLong("zjRaas4g+CQfTc1I/nrxbA==")
+
     it("should have answers for part 1") {
       part1(input: _*) shouldBe answer1
     }
 
     it("should have answers for part 2") {
-      part2(input: _*) shouldBe answer2
+      part2(1000000, input: _*) shouldBe answer2
     }
   }
 }
