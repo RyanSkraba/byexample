421a3377fe599c7f9208d0f68e1e13cffedf761b
Ryan Skraba <ryan@skraba.com>
2024-07-16 08:36:23 +0200

    feat: Add boolean cfg values

--- scala/ammonite-by-example/src/test/scala/com/skraba/byexample/scala/ammonite/validator/CfgSpec.scala
+++ scala/ammonite-by-example/src/test/scala/com/skraba/byexample/scala/ammonite/validator/CfgSpec.scala
@@ -7,7 +7,8 @@ import org.scalatest.matchers.should.Matchers
 /** Test the [[Cfg]] instances. */
 class CfgSpec extends AnyFunSpecLike with Matchers with BeforeAndAfterAll {
 
-  val props: Map[String, String] = Map("string" -> "ValueFromMap", "empty" -> "")
+  val props: Map[String, String] =
+    Map("string" -> "ValueFromMap", "empty" -> "", "booleanFalse" -> "false", "booleanTrue" -> "true")
 
   describe("String configuration using Cfg.str") {
     it("should take any explicitly set value") {
@@ -32,6 +33,51 @@ class CfgSpec extends AnyFunSpecLike with Matchers with BeforeAndAfterAll {
       val cfg = Cfg.str(None, props, "no-exist", "Doc")("default")
       cfg.get shouldBe "default"
       cfg.isDefault shouldBe true
+    }
+
+    it("should detect the default regardless of how it finds the value") {
+      Cfg.str(Some("Explicit"), props, "string", "Doc")("Explicit").isDefault shouldBe true
+      Cfg.str(None, props, "string", "Doc")("ValueFromMap").isDefault shouldBe true
+      Cfg.str(None, props, "empty", "Doc")("").isDefault shouldBe true
+      Cfg.str(None, props, "no-exist", "Doc")("default").isDefault shouldBe true
+    }
+  }
+
+  describe("Boolean configuration using Cfg.bool") {
+    it("should take any explicitly set value") {
+      val cfg1 = Cfg.bool(Some(true), props, "booleanTrue", "Doc")(d = false)
+      cfg1.get shouldBe true
+      cfg1.isDefault shouldBe false
+      val cfg2 = Cfg.bool(Some(false), props, "booleanFalse", "Doc")(d = true)
+      cfg2.get shouldBe false
+      cfg2.isDefault shouldBe false
+    }
+
+    it("should fetch values from properties") {
+      val cfg1 = Cfg.bool(None, props, "booleanTrue", "Doc")(d = false)
+      cfg1.get shouldBe true
+      cfg1.isDefault shouldBe false
+      val cfg2 = Cfg.bool(None, props, "booleanFalse", "Doc")(d = true)
+      cfg2.get shouldBe false
+      cfg2.isDefault shouldBe false
+    }
+
+    it("should fall back to a default value when the property doesn't exist") {
+      val cfg1 = Cfg.bool(None, props, "no-exist", "Doc")(d = true)
+      cfg1.get shouldBe true
+      cfg1.isDefault shouldBe true
+      val cfg2 = Cfg.bool(None, props, "no-exist", "Doc")(d = false)
+      cfg2.get shouldBe false
+      cfg2.isDefault shouldBe true
+    }
+
+    it("should detect the default regardless of how it finds the value") {
+      Cfg.bool(Some(true), props, "booleanFalse", "Doc")(d = true).isDefault shouldBe true
+      Cfg.bool(Some(false), props, "booleanTrue", "Doc")(d = false).isDefault shouldBe true
+      Cfg.bool(None, props, "booleanTrue", "Doc")(d = true).isDefault shouldBe true
+      Cfg.bool(None, props, "booleanFalse", "Doc")(d = false).isDefault shouldBe true
+      Cfg.bool(None, props, "no-exist", "Doc")(d = true).isDefault shouldBe true
+      Cfg.bool(None, props, "no-exist", "Doc")(d = false).isDefault shouldBe true
     }
   }
 }
