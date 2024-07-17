a458853181f78ebe5eb877f5cd7a7e98eaae090f
Ryan Skraba <ryan@skraba.com>
2024-07-17 07:43:55 +0200

    feat: Add path cfg values

--- scala/ammonite-by-example/src/test/scala/com/skraba/byexample/scala/ammonite/validator/CfgSpec.scala
+++ scala/ammonite-by-example/src/test/scala/com/skraba/byexample/scala/ammonite/validator/CfgSpec.scala
@@ -8,7 +8,14 @@ import org.scalatest.matchers.should.Matchers
 class CfgSpec extends AnyFunSpecLike with Matchers with BeforeAndAfterAll {
 
   val props: Map[String, String] =
-    Map("string" -> "ValueFromMap", "empty" -> "", "booleanFalse" -> "false", "booleanTrue" -> "true")
+    Map(
+      "string" -> "ValueFromMap",
+      "empty" -> "",
+      "booleanFalse" -> "false",
+      "booleanTrue" -> "true",
+      "pathRel" -> "propsRel",
+      "pathAbs" -> "/props/absolute"
+    )
 
   describe("String configuration using Cfg.str") {
     it("should take any explicitly set value") {
@@ -78,6 +85,43 @@ class CfgSpec extends AnyFunSpecLike with Matchers with BeforeAndAfterAll {
       Cfg.bool(None, props, "booleanFalse", "Doc")(d = false).isDefault shouldBe true
       Cfg.bool(None, props, "no-exist", "Doc")(d = true).isDefault shouldBe true
       Cfg.bool(None, props, "no-exist", "Doc")(d = false).isDefault shouldBe true
+    }
+  }
+
+  describe("path configuration using Cfg.path") {
+    val base = os.Path("/base")
+    val other = os.Path("/other")
+
+    it("should take any explicitly set value") {
+      val cfgRel = Cfg.path(base, Some("relative"), props, "pathAbs", "Doc")(other)
+      cfgRel.get shouldBe base / "relative"
+      cfgRel.isDefault shouldBe false
+      val cfgAbs = Cfg.path(base, Some("/path/absolute"), props, "pathRel", "Doc")(other)
+      cfgAbs.get shouldBe os.Path("/path/absolute")
+      cfgRel.isDefault shouldBe false
+    }
+
+    it("should fetch values from properties") {
+      val cfgRel = Cfg.path(base, None, props, "pathRel", "Doc")(other)
+      cfgRel.get shouldBe base / "propsRel"
+      cfgRel.isDefault shouldBe false
+      val cfgAbs = Cfg.path(base, None, props, "pathAbs", "Doc")(other)
+      cfgAbs.get shouldBe os.Path("/props/absolute")
+      cfgRel.isDefault shouldBe false
+    }
+
+    it("should fall back to a default value when the property doesn't exist") {
+      val cfg = Cfg.path(base, None, props, "no-exist", "Doc")(other)
+      cfg.get shouldBe other
+      cfg.isDefault shouldBe true
+    }
+
+    it("should detect the default regardless of how it finds the value") {
+      Cfg.path(base, Some("relative"), props, "pathAbs", "Doc")(base / "relative").isDefault shouldBe true
+      Cfg.path(base, Some("/path/absolute"), props, "pathRel", "Doc")(os.Path("/path/absolute")).isDefault shouldBe true
+      Cfg.path(base, None, props, "pathRel", "Doc")(base / "propsRel").isDefault shouldBe true
+      Cfg.path(base, None, props, "pathAbs", "Doc")(os.Path("/props/absolute")).isDefault shouldBe true
+      Cfg.path(base, None, props, "no-exist", "Doc")(other).isDefault shouldBe true
     }
   }
 }
