cdf8102fc9b73c972439d3f458935bcbf473f669
Ryan Skraba <ryan@skraba.com>
2024-07-18 07:24:15 +0200

    feat: Rework configurations for different name

---
+++ scala/ammonite-by-example/src/main/scala/com/skraba/byexample/scala/ammonite/validator/Setting.scala
@@ -0,0 +1,111 @@
+package com.skraba.byexample.scala.ammonite.validator
+
+import com.skraba.docoptcli.AnsiConsole
+
+/** A setting configures a named value, like a variable or context or configuration. There are many libraries that do
+  * this. This is mine for ammonite.
+  *
+  * @param v
+  *   An optional explicit value to be assigned to this setting. This is usually done programmatically or from the
+  *   command line.
+  * @param props
+  *   A list of properties that can be used to determine the value of a setting in this system by looking it up by the
+  *   propKey.
+  * @param propKey
+  *   The property key (or name) that is linked to this value.
+  * @param propFn
+  *   A function that turns the string (if any) in the properties into the correct value type, but only if properties
+  *   are being used to construct the value.
+  * @param doc
+  *   Some user-friendly documentation for how the setting value is used.
+  * @param defaultFn
+  *   A function to generate the default value for this setting.
+  * @tparam T
+  *   The type of the setting value.
+  */
+case class Setting[T](
+    v: Option[T],
+    props: Map[String, String],
+    propKey: String,
+    propFn: String => T,
+    doc: String
+)(
+    defaultFn: => T
+) {
+
+  /** Evaluate the default value once. */
+  lazy val d = defaultFn
+
+  /** True if this value is the default. */
+  lazy val isDefault: Boolean = get.equals(d)
+
+  /** The evaluated value, either the explicit value, from the properties or the default. */
+  lazy val get: T = v.orElse(props.get(propKey).map(propFn)).getOrElse(d)
+
+  override def toString: String = get.toString
+
+  /** Provides a colourful representation of the value, notably different if the default or non-default is used. */
+  def toString(cfg: AnsiConsole): String = if (isDefault) cfg.green(this) else cfg.right(this)
+}
+
+object Setting {
+
+  /** @param v
+    *   An optional explicit string value to be assigned to this setting.
+    * @param props
+    *   A list of properties that can be used to determine the value of a setting in this system by looking it up by the
+    *   propKey.
+    * @param propKey
+    *   The property key (or name) that is linked to this value.
+    * @param doc
+    *   Some user-friendly documentation for how the setting value is used.
+    * @param d
+    *   A function to generate the default string value for this setting.
+    */
+  def str(v: Option[String], props: Map[String, String], propKey: String, doc: String)(
+      d: => String = ""
+  ): Setting[String] =
+    Setting(v, props, propKey, identity, doc)(d)
+
+  /** @param v
+    *   An optional explicit boolean value to be assigned to this setting.
+    * @param props
+    *   A list of properties that can be used to determine the value of a setting in this system by looking it up by the
+    *   propKey.
+    * @param propKey
+    *   The property key (or name) that is linked to this value.
+    * @param doc
+    *   Some user-friendly documentation for how the setting value is used.
+    * @param d
+    *   A function to generate the default boolean value for this setting.
+    */
+  def bool(v: Option[Boolean], props: Map[String, String], propKey: String, doc: String)(
+      d: => Boolean = false
+  ): Setting[Boolean] =
+    Setting(v, props, propKey, _.toBoolean, doc)(d)
+
+  /** @param base
+    *   The base directory to use in case the discovered value is not an absoluate path.
+    * @param v
+    *   An optional explicit string value to be assigned to this setting as a path, resolving if necessary.
+    * @param props
+    *   A list of properties that can be used to determine the value of a setting in this system by looking it up by the
+    *   propKey.
+    * @param propKey
+    *   The property key (or name) that is linked to this value. If relative, this will be resolved from the base
+    *   directory.
+    * @param doc
+    *   Some user-friendly documentation for how the setting value is used.
+    * @param d
+    *   A function to generate the default path value for this setting.
+    */
+  def path(base: os.Path, v: Option[String], props: Map[String, String], propKey: String, doc: String)(
+      d: => os.Path
+  ): Setting[os.Path] =
+    Setting(v.map(os.FilePath(_).resolveFrom(base)), props, propKey, p => os.FilePath(p).resolveFrom(base), doc)(
+      d
+    )
+
+  def properties(cfg: AnsiConsole, cfgs: Setting[?]*): String =
+    cfgs.map(c => s"${cfg.left(c.propKey)}=${c.toString(cfg)}").mkString("\n")
+}
---
+++ scala/ammonite-by-example/src/test/scala/com/skraba/byexample/scala/ammonite/validator/SettingSpec.scala
@@ -0,0 +1,127 @@
+package com.skraba.byexample.scala.ammonite.validator
+
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.funspec.AnyFunSpecLike
+import org.scalatest.matchers.should.Matchers
+
+/** Test the [[Setting]] instances. */
+class SettingSpec extends AnyFunSpecLike with Matchers with BeforeAndAfterAll {
+
+  val props: Map[String, String] =
+    Map(
+      "string" -> "ValueFromMap",
+      "empty" -> "",
+      "booleanFalse" -> "false",
+      "booleanTrue" -> "true",
+      "pathRel" -> "propsRel",
+      "pathAbs" -> "/props/abs"
+    )
+
+  describe("String configuration using Setting.str") {
+    it("should take any explicitly set value") {
+      val x = Setting.str(Some("Explicit"), props, "string", "Doc")("default")
+      x.get shouldBe "Explicit"
+      x.isDefault shouldBe false
+    }
+
+    it("should fetch values from properties") {
+      val x = Setting.str(None, props, "string", "Doc")("default")
+      x.get shouldBe "ValueFromMap"
+      x.isDefault shouldBe false
+    }
+
+    it("should even fetch values if they are empty") {
+      val x = Setting.str(None, props, "empty", "Doc")("default")
+      x.get shouldBe ""
+      x.isDefault shouldBe false
+    }
+
+    it("should fall back to a default value when the property doesn't exist") {
+      val x = Setting.str(None, props, "no-exist", "Doc")("default")
+      x.get shouldBe "default"
+      x.isDefault shouldBe true
+    }
+
+    it("should detect the default regardless of how it finds the value") {
+      Setting.str(Some("Explicit"), props, "string", "Doc")("Explicit").isDefault shouldBe true
+      Setting.str(None, props, "string", "Doc")("ValueFromMap").isDefault shouldBe true
+      Setting.str(None, props, "empty", "Doc")("").isDefault shouldBe true
+      Setting.str(None, props, "no-exist", "Doc")("default").isDefault shouldBe true
+    }
+  }
+
+  describe("Boolean configuration using Setting.bool") {
+    it("should take any explicitly set value") {
+      val x1 = Setting.bool(Some(true), props, "booleanTrue", "Doc")(d = false)
+      x1.get shouldBe true
+      x1.isDefault shouldBe false
+      val x2 = Setting.bool(Some(false), props, "booleanFalse", "Doc")(d = true)
+      x2.get shouldBe false
+      x2.isDefault shouldBe false
+    }
+
+    it("should fetch values from properties") {
+      val x1 = Setting.bool(None, props, "booleanTrue", "Doc")(d = false)
+      x1.get shouldBe true
+      x1.isDefault shouldBe false
+      val x2 = Setting.bool(None, props, "booleanFalse", "Doc")(d = true)
+      x2.get shouldBe false
+      x2.isDefault shouldBe false
+    }
+
+    it("should fall back to a default value when the property doesn't exist") {
+      val x1 = Setting.bool(None, props, "no-exist", "Doc")(d = true)
+      x1.get shouldBe true
+      x1.isDefault shouldBe true
+      val x2 = Setting.bool(None, props, "no-exist", "Doc")(d = false)
+      x2.get shouldBe false
+      x2.isDefault shouldBe true
+    }
+
+    it("should detect the default regardless of how it finds the value") {
+      Setting.bool(Some(true), props, "booleanFalse", "Doc")(d = true).isDefault shouldBe true
+      Setting.bool(Some(false), props, "booleanTrue", "Doc")(d = false).isDefault shouldBe true
+      Setting.bool(None, props, "booleanTrue", "Doc")(d = true).isDefault shouldBe true
+      Setting.bool(None, props, "booleanFalse", "Doc")(d = false).isDefault shouldBe true
+      Setting.bool(None, props, "no-exist", "Doc")(d = true).isDefault shouldBe true
+      Setting.bool(None, props, "no-exist", "Doc")(d = false).isDefault shouldBe true
+    }
+  }
+
+  describe("path configuration using Setting.path") {
+    val base = os.Path("/base")
+    val other = os.Path("/other")
+
+    it("should take any explicitly set value") {
+      val xRel = Setting.path(base, Some("rel"), props, "pathAbs", "Doc")(other)
+      xRel.get shouldBe base / "rel"
+      xRel.isDefault shouldBe false
+      val xAbs = Setting.path(base, Some("/path/abs"), props, "pathRel", "Doc")(other)
+      xAbs.get shouldBe os.Path("/path/abs")
+      xAbs.isDefault shouldBe false
+    }
+
+    it("should fetch values from properties") {
+      val xRel = Setting.path(base, None, props, "pathRel", "Doc")(other)
+      xRel.get shouldBe base / "propsRel"
+      xRel.isDefault shouldBe false
+      val xAbs = Setting.path(base, None, props, "pathAbs", "Doc")(other)
+      xAbs.get shouldBe os.Path("/props/abs")
+      xAbs.isDefault shouldBe false
+    }
+
+    it("should fall back to a default value when the property doesn't exist") {
+      val x = Setting.path(base, None, props, "no-exist", "Doc")(other)
+      x.get shouldBe other
+      x.isDefault shouldBe true
+    }
+
+    it("should detect the default regardless of how it finds the value") {
+      Setting.path(base, Some("rel"), props, "pathAbs", "Doc")(base / "rel").isDefault shouldBe true
+      Setting.path(base, Some("/path/abs"), props, "pathRel", "Doc")(os.Path("/path/abs")).isDefault shouldBe true
+      Setting.path(base, None, props, "pathRel", "Doc")(base / "propsRel").isDefault shouldBe true
+      Setting.path(base, None, props, "pathAbs", "Doc")(os.Path("/props/abs")).isDefault shouldBe true
+      Setting.path(base, None, props, "no-exist", "Doc")(other).isDefault shouldBe true
+    }
+  }
+}
