03bf2a7aabfe392703b2a4bd123aa3002856731a
Ryan Skraba <ryan@skraba.com>
2024-07-31 08:34:06 +0200

    feat: Add class for generating video title cards

---
+++ scala/ammonite-by-example/src/main/scala/com/skraba/byexample/scala/ammonite/video/Card.scala
@@ -0,0 +1,99 @@
+package com.skraba.byexample.scala.ammonite.video
+
+import com.skraba.byexample.scala.ammonite.FileMaker
+import com.skraba.docoptcli.AnsiConsole
+
+/** A card is an image that should be applied to the beginning or the end of a video.
+  * @param genSvg
+  *   A generator to create the SVG file for this card
+  * @param filename
+  *   The filename to use for the different files, which will be appended with the appropriate extension.
+  * @param maker
+  *   A helper configured to create the files lazily
+  * @param dirSvg
+  *   The SVG output directory
+  * @param dirPng
+  *   The PNG output directory
+  * @param dirMp4
+  *   The MP4 output directory
+  * @param duration
+  *   The length of the output video (in seconds)
+  */
+case class Card(
+    genSvg: AnsiConsole => String,
+    filename: String,
+    maker: FileMaker = FileMaker(),
+    dirSvg: os.Path,
+    dirPng: os.Path,
+    dirMp4: os.Path,
+    duration: Int = 5
+) {
+
+  /** The title card in SVG format. */
+  val svg: FileMaker = maker.blue(dstPath = dirSvg / (filename + ".svg"), tag = "svg") {
+    os.write.over(_, genSvg(maker.out))
+  }
+
+  /** The title card in PNG format. */
+  val png: FileMaker = maker
+    .green(dstPath = dirPng / (filename + ".png"), tag = "png") {
+      Ffmpeg().osProc("inkscape", "-w", "1920", "-h", "1080", svg.dst.get, "--export-filename", _)
+    }
+
+  private lazy val mp4Cache = collection.mutable.Map[Int, FileMaker]()
+
+  /** The title card in MP4 format. */
+  def mp4(ff: Ffmpeg, frameRate: Int = 25): FileMaker = mp4Cache.getOrElseUpdate(
+    frameRate, {
+      val mp4Path = dirMp4 / (filename + (if (frameRate == 25) "" else s".${frameRate}fps") + ".mp4")
+      maker.magenta(mp4Path, "mp4") { ff.mp4(_).pngToMp4(png.dst.get, frameRate = frameRate, duration = duration) }
+    }
+  )
+}
+
+object Card {
+
+  /** Given a template string, searches and replaces some lines in the template.
+    *
+    * The tags in the template look like `[[TAG4.3]]` where the first digit is the total number of lines that were found
+    * and the second digit is to be replaced by the n'th line.
+    *
+    * For example, if there are three incoming lines `[[TAG3.1]]`, `[[TAG3.2]]`, `[[TAG3.3]]` will be replaced.
+    *
+    * If there is one incoming line, `[[TAG]]`, `[[TAG1]]`, and `[[TAG1.1]]` are all replaced.
+    *
+    * Any of the unreplaced tags are automatically deleted from the template.
+    *
+    * @param tmpl
+    *   The incoming string to be replaced.
+    * @param tag
+    *   The tag to use when looking up in the template.
+    * @param lines
+    *   The lines to replace in the template.
+    * @return
+    *   The result of all the replacements.
+    */
+  def multiReplaceInTemplate(tmpl: String, tag: String, lines: Seq[String]): String = {
+    val tagRe = s"\\[\\[($tag(\\d*)(\\.\\d*)?)]]".r
+
+    val max = tagRe.findAllMatchIn(tmpl).map(_.group(2)).filterNot(_.isEmpty).map(_.toInt).max
+
+    val replaced = lines.size match {
+      case 1 =>
+        // If there's only one line incoming, then replace [[TAG]], [[TAG1]] or [[TAG1.1]]
+        tmpl
+          .replaceAll(s"\\[\\[$tag]]", lines.head)
+          .replaceAll(s"\\[\\[${tag}1]]", lines.head)
+          .replaceAll(s"\\[\\[${tag}1.1]]", lines.head)
+      case size if size <= max =>
+        // Otherwise replace [[TAGM.N]] (e.g.  [[TAG2.1]] and [[TAG2.2]] for two lines)
+        lines.zipWithIndex.foldLeft(tmpl) { case (in, (line, lineNum)) =>
+          in.replaceAll(s"\\[\\[$tag$size.${lineNum + 1}]]", line)
+        }
+      case _ => throw new IllegalStateException(s"Weird $tag: " + lines)
+    }
+
+    tagRe.replaceAllIn(replaced, "")
+  }
+
+}
---
+++ scala/ammonite-by-example/src/test/scala/com/skraba/byexample/scala/ammonite/video/CardSpec.scala
@@ -0,0 +1,105 @@
+package com.skraba.byexample.scala.ammonite.video
+
+import com.skraba.byexample.scala.ammonite.OsPathScalaRelectIOConverters._
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.funspec.AnyFunSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import scala.reflect.io.Directory
+
+/** Test the [[Card]] class. */
+class CardSpec extends AnyFunSpecLike with BeforeAndAfterAll with Matchers {
+
+  /** A temporary directory for playing with files. */
+  val Tmp: Directory = Directory.makeTemp(getClass.getSimpleName)
+
+  /** And delete temporary resources after the script. */
+  override protected def afterAll(): Unit =
+    try {
+      Tmp.deleteRecursively()
+    } catch {
+      case ex: Exception =>
+        ex.printStackTrace()
+    }
+
+  describe("Create a basic svg file using the card") {
+    it("should create the files as needed") {
+      val flag = Card(
+        genSvg = _ => CardSpec.SvgFlag,
+        filename = "canada_flag",
+        dirSvg = Tmp / "svg",
+        dirPng = Tmp / "png",
+        dirMp4 = Tmp / "mp4"
+      )
+
+      // Files are created lazily as they are accessed in the card
+      (Tmp / "svg").jfile shouldNot exist
+      (Tmp / "png").jfile shouldNot exist
+      (Tmp / "mp4").jfile shouldNot exist
+
+      // The SVG file
+      flag.svg.dst
+      (Tmp / "svg").jfile should exist
+      (Tmp / "png").jfile shouldNot exist
+      (Tmp / "mp4").jfile shouldNot exist
+      (Tmp / "svg" / "canada_flag.svg").jfile should exist
+
+      // The SVG file
+      flag.png.dst
+      (Tmp / "svg").jfile should exist
+      (Tmp / "png").jfile should exist
+      (Tmp / "mp4").jfile shouldNot exist
+      (Tmp / "svg" / "canada_flag.svg").jfile should exist
+      (Tmp / "png" / "canada_flag.png").jfile should exist
+
+      flag.mp4(Ffmpeg(), 25).dst
+      (Tmp / "svg").jfile should exist
+      (Tmp / "png").jfile should exist
+      (Tmp / "mp4").jfile should exist
+      (Tmp / "svg" / "canada_flag.svg").jfile should exist
+      (Tmp / "png" / "canada_flag.png").jfile should exist
+      (Tmp / "mp4" / "canada_flag.mp4").jfile should exist
+
+      flag.mp4(Ffmpeg(), 30).dst
+      (Tmp / "svg").jfile should exist
+      (Tmp / "png").jfile should exist
+      (Tmp / "mp4").jfile should exist
+      (Tmp / "svg" / "canada_flag.svg").jfile should exist
+      (Tmp / "png" / "canada_flag.png").jfile should exist
+      (Tmp / "mp4" / "canada_flag.mp4").jfile should exist
+      (Tmp / "mp4" / "canada_flag.30fps.mp4").jfile should exist
+    }
+  }
+}
+
+object CardSpec {
+
+  /** SVG containing the canada flag. */
+  val SvgFlag: String = """<svg xmlns="http://www.w3.org/2000/svg" width="1200" height="600" viewBox="0 0 9600 4800">
+    | <!-- https://upload.wikimedia.org/wikipedia/commons/c/cf/Flag_of_Canada.svg -->
+    |	<title>Flag of Canada</title>
+    |	<path fill="#f00" d="m0 0h2400l99 99h4602l99-99h2400v4800h-2400l-99-99h-4602l-99 99H0z"/>
+    |	<path fill="#fff"
+    |     d="m2400 0h4800v4800h-4800zm2490 4430-45-863a95 95 0 0 1 111-98l859 151-116-320a65 65 0 0
+    |        1 20-73l941-762-212-99a65 65 0 0 1-34-79l186-572-542 115a65 65 0 0 1-73-38l-105-247-423
+    |        454a65 65 0 0 1-111-57l204-1052-327 189a65 65 0 0 1-91-27l-332-652-332 652a65 65 0 0
+    |        1-91 27l-327-189 204 1052a65 65 0 0 1-111 57l-423-454-105 247a65 65 0 0 1-73 38l-542-115
+    |        186 572a65 65 0 0 1-34 79l-212 99 941 762a65 65 0 0 1 20 73l-116 320 859-151a95 95 0 0
+    |        1 111 98l-45 863z"/>
+    |</svg>""".stripMargin
+
+  /** @param t
+    *   A time value where the rectangle starts in the upper left at zero and moves to the lower left at 1 second and
+    *   back at 2 second
+    * @return
+    *   An svg with a red rectangle moving from the upper right to lower left as time passes from 0 to 1.
+    */
+  def svg(t: Double, dx: Double = 1920, dy: Double = 1080, pixelDx: Double = 50): String = {
+    val it = t.toLong
+    val dt = if (it % 2 == 0) t - it else 1 + it - t
+    s"""<svg xmlns="http://www.w3.org/2000/svg" width="$dx" height="$dy" viewBox="0 0 $dx $dy">
+      |	<rect fill="#fee" width="$dx" height="$dy"/>
+      |	<rect fill="#f00" x="${dt * (dx - pixelDx)}" y="${dt * (dy - pixelDx)}" width="10" height="10"/>
+      |</svg>""".stripMargin
+  }
+}
