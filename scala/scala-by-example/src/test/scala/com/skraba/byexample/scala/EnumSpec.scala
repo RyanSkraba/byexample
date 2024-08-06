77008bb7e0c3e036bcc044cbdc78cbbf99e63cb5
Ryan Skraba <ryan@skraba.com>
2024-08-06 08:05:23 +0200

    feat: Add a test for Scala2 enum

---
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/EnumSpec.scala
@@ -0,0 +1,30 @@
+package com.skraba.byexample.scala
+
+import org.scalatest.funspec.AnyFunSpecLike
+import org.scalatest.matchers.should.Matchers
+
+/** Using [[Enumeration]] in Scala. These are significantly improved in Scala 3! */
+class EnumSpec extends AnyFunSpecLike with Matchers {
+
+  /** A basic enumeration with just values. */
+  object Dir extends Enumeration {
+    type Dir = Value
+    val East, South, West, North = Value
+  }
+  import Dir._
+
+  describe("Using a basic enum") {
+    describe("when converting from strings") {
+
+      it("should generate a string correctly") { West.toString shouldBe "West" }
+
+      it("should interpret a string correctly") { Dir.withName("East") shouldBe East }
+
+      it("should throw an exception on a bad string") {
+        intercept[NoSuchElementException] {
+          Dir.withName("Norst")
+        }.getMessage shouldBe "No value found for 'Norst'"
+      }
+    }
+  }
+}
