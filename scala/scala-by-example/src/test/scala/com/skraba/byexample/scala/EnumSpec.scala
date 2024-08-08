5885ab8326379af0e593e050d0a8a9953f695e59
Ryan Skraba <ryan@skraba.com>
2024-08-08 08:20:42 +0200

    feat: Add ordinals to Enum test

--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/EnumSpec.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/EnumSpec.scala
@@ -15,7 +15,12 @@ class EnumSpec extends AnyFunSpecLike with Matchers {
   }
   import Dir._
 
-  describe("Using a basic enum") {
+  describe("Using a basic Dir enum") {
+
+    it("should have a max value") { Dir.maxId shouldBe 4 }
+
+    it("should list its values") { Dir.values shouldBe Set(East, South, West, North) }
+
     describe("when converting from strings") {
 
       it("should generate a string correctly") { West.toString shouldBe "West" }
@@ -29,6 +34,21 @@ class EnumSpec extends AnyFunSpecLike with Matchers {
       }
 
       it("should wrap in Try to avoid an exception") { Try(Dir.withName("Norst")).toOption shouldBe None }
+    }
+
+    describe("when converting as ordinal integers") {
+
+      it("should generate an integer correctly") { West.id shouldBe 2 }
+
+      it("should interpret a integer correctly") { Dir(1) shouldBe South }
+
+      it("should throw an exception on a bad string") {
+        intercept[NoSuchElementException] {
+          Dir(100)
+        }.getMessage shouldBe "key not found: 100"
+      }
+
+      it("should wrap in Try to avoid an exception") { Try(Dir(100)).toOption shouldBe None }
     }
   }
 }
