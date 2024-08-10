347c52d94ccba83b6fa2bb6d4117eb95f4e743aa
Ryan Skraba <ryan@skraba.com>
2024-08-10 10:50:35 +0200

    feat: Example enum-style sealed traits

--- scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/EnumSpec.scala
+++ scala/scala-by-example/src/test/scala/com/skraba/byexample/scala/EnumSpec.scala
@@ -27,6 +27,37 @@ class EnumSpec extends AnyFunSpecLike with Matchers {
     val Sophia: MyVal = MyVal("Estelle Getty", "July 25, 1923")
   }
 
+  /** As an alternative to the enum, scala devs often use sealed traits. Note that some of the implementation (such as
+    * the id and number) needs to be manually implemented if required.
+    */
+  sealed trait Greeter {
+    def id: Int
+
+    def greeting(name: String): String
+  }
+
+  object Greeter {
+
+    case object Friendly extends Greeter { val id = 0; def greeting(name: String): String = s"Hello $name!" }
+
+    case object Casual extends Greeter { val id = 1; def greeting(name: String): String = s"Hi $name." }
+
+    case object Formal extends Greeter { val id = 2; def greeting(name: String): String = s"Salutations, $name." }
+    case object Aimable extends Greeter { val id = 3; def greeting(name: String): String = s"Bonjour $name!" }
+
+    val maxId = 4
+
+    def values: Set[Greeter] = Set(Friendly, Casual, Formal, Aimable)
+
+    def apply(i: Int): Greeter = values.find(_.id == i).getOrElse {
+      throw new NoSuchElementException(s"key not found: $i")
+    }
+
+    def withName(s: String): Greeter = values.find(_.toString == s).getOrElse {
+      throw new NoSuchElementException(s"No value found for '$s'")
+    }
+  }
+
   describe("Using a basic Dir enum") {
 
     import Dir._
@@ -86,7 +117,7 @@ class EnumSpec extends AnyFunSpecLike with Matchers {
         }.getMessage shouldBe "No value found for 'Stan'"
       }
 
-      it("should wrap in Try to avoid an exception") { Try(Dir.withName("Stan")).toOption shouldBe None }
+      it("should wrap in Try to avoid an exception") { Try(GoldenGirl.withName("Stan")).toOption shouldBe None }
     }
 
     describe("when converting as ordinal integers") {
@@ -108,6 +139,51 @@ class EnumSpec extends AnyFunSpecLike with Matchers {
       it("should generate an integer correctly") {
         Rose.actress shouldBe "Betty White"
         Rose.birthday shouldBe "January 17, 1922"
+      }
+    }
+  }
+
+  describe("Using a sealed trait Greeting") {
+
+    import Greeter._
+
+    it("should have a max value") { Greeter.maxId shouldBe 4 }
+
+    it("should list its values") { Greeter.values shouldBe Set(Friendly, Casual, Formal, Aimable) }
+
+    describe("when converting from strings") {
+
+      it("should generate a string correctly") { Casual.toString shouldBe "Casual" }
+
+      it("should interpret a string correctly") { Greeter.withName("Formal") shouldBe Formal }
+
+      it("should throw an exception on a bad string") {
+        intercept[NoSuchElementException] {
+          Greeter.withName("Aggro")
+        }.getMessage shouldBe "No value found for 'Aggro'"
+      }
+
+      it("should wrap in Try to avoid an exception") { Try(Greeter.withName("Aggro")).toOption shouldBe None }
+    }
+
+    describe("when converting as ordinal integers") {
+
+      it("should generate an integer correctly") { Aimable.id shouldBe 3 }
+
+      it("should interpret a integer correctly") { Greeter(1) shouldBe Casual }
+
+      it("should throw an exception on a bad string") {
+        intercept[NoSuchElementException] {
+          Greeter(100)
+        }.getMessage shouldBe "key not found: 100"
+      }
+
+      it("should wrap in Try to avoid an exception") { Try(Greeter(100)).toOption shouldBe None }
+    }
+
+    describe("when getting enum data") {
+      it("should generate an integer correctly") {
+        Aimable.greeting("Betty") shouldBe "Bonjour Betty!"
       }
     }
   }
