9418fde0a8a56c3543cf2d75fb0b8a469447662d
Ryan Skraba <ryan@skraba.com>
2024-10-12 09:42:29 +0200

    feat: Clarification of custom conditions

--- testing/assertj-by-example/src/test/java/com/skraba/byexample/assertj/CustomPredicateTest.java
+++ testing/assertj-by-example/src/test/java/com/skraba/byexample/assertj/CustomPredicateTest.java
@@ -1,41 +1,44 @@
 package com.skraba.byexample.assertj;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 
-/** Simple assertions using a custom predicate. */
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+/**
+ * Custom assertions using {@link Condition}, and lambda
+ */
 class CustomPredicateTest {
 
+  String abc1234 = "ABC-1234";
+
   @Test
-  void testBasicStringCondition() {
-    assertThat("ABC-1234").is(new CountDashes(1));
-    assertThatExceptionOfType(AssertionError.class)
-        .isThrownBy(() -> assertThat("ABC-1234").is(new CountDashes(2)))
-        .withMessageEndingWith("to be with 2 dashes");
+  void testCustomConditions() {
+    // is, has, satisfies are all aliases to check a Condition. The description is in the custom Condition.
+    var singleDash = new CountDashes(1);
+    var doubleDash = new CountDashes(2);
+    assertThat(abc1234).is(singleDash).has(singleDash).satisfies(singleDash).isNot(doubleDash).doesNotHave(doubleDash);
+    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> assertThat("ABC-1234").is(doubleDash)).withMessageEndingWith("to be with 2 dashes");
   }
 
   @Test
   void testBasicStringConditionAsLambda() {
-    // expressed as a lambda, requires a description
-    assertThat("ABC-1234").is(new Condition<>(v -> v.length() == 8, "size is 8"));
-    assertThatExceptionOfType(AssertionError.class)
-        .isThrownBy(
-            () -> assertThat("ABCD-1234").is(new Condition<>(v -> v.length() == 8, "size is 8")))
-        .withMessageEndingWith("to be size is 8");
+    // expressed as a lambda in a Condition, requires a description.
+    assertThat(abc1234).is(new Condition<>(v -> v.length() == 8, "size is 8"));
+    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> assertThat("ABCD-1234").is(new Condition<>(v -> v.length() == 8, "size is 8"))).withMessageEndingWith("to be size is 8");
   }
 
   @Test
   void testBasicStringPredicateAsLambda() {
-    assertThat("ABC-1234").matches(v -> v.length() == 8, "size is 8");
-    assertThatExceptionOfType(AssertionError.class)
-        .isThrownBy(() -> assertThat("ABCD-1234").matches(v -> v.length() == 8, "size is 8"))
-        .withMessageEndingWith("to match 'size is 8' predicate.");
+    // expressed as a lambda predicate (returning true for success), requires a description.
+    assertThat(abc1234).matches(v -> v.length() == 8, "size is 8");
+    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> assertThat("ABCD-1234").matches(v -> v.length() == 8, "size is 8")).withMessageEndingWith("to match 'size is 8' predicate.");
   }
 
-  /** Condition counting the number of dashes in the string. */
+  /**
+   * A custom Condition counting the number of dashes in the string.
+   */
   public static class CountDashes extends Condition<String> {
 
     private final int expected;
