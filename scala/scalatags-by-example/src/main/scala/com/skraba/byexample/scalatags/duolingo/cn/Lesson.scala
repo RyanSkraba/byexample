767da65adb4ddce2a6fccc267188998a0be45f1b
Ryan Skraba <ryan@skraba.com>
2024-10-14 07:43:01 +0200

    feat: Fix syllable markers

--- scala/scalatags-by-example/src/main/scala/com/skraba/byexample/scalatags/duolingo/cn/Lesson.scala
+++ scala/scalatags-by-example/src/main/scala/com/skraba/byexample/scalatags/duolingo/cn/Lesson.scala
@@ -69,25 +69,25 @@ object Lesson {
 
   val FruitsAndVegetables: Lesson = Lesson(
     "Fruits and Vegetables",
-    Vocab("苹果:pi²ngguo³:apple"),
-    Vocab("香蕉:xia¹ngjia¹o:banana"),
-    Vocab("花椰菜:hua¹ye²ca⁴i:broccoli"),
+    Vocab("苹果:ping²guo³:apple"),
+    Vocab("香蕉:xiang¹jiao¹:banana"),
+    Vocab("花椰菜:hua¹ye²cai⁴:broccoli"),
     Vocab("胡萝卜:hu²luo²bo:carrot"),
-    Vocab("白菜:ba²ica⁴i:chinese cabbage"),
-    Vocab("黄瓜:hua²nggua¹:cucumber"),
+    Vocab("白菜:bai²cai⁴:chinese cabbage"),
+    Vocab("黄瓜:huang²gua¹:cucumber"),
     Vocab("茄子:qie²zi:eggplant"),
-    Vocab("大蒜:da⁴sua⁴n:garlic"),
+    Vocab("大蒜:da⁴suan⁴:garlic"),
     Vocab("葡萄:pu²tao:grape"),
-    Vocab("青椒:qi¹ngjia¹o:green pepper"),
-    Vocab("芒果:ma²ngguo³:mango"),
+    Vocab("青椒:qing¹jiao¹:green pepper"),
+    Vocab("芒果:mang²guo³:mango"),
     Vocab("蘑菇:mo²gu:mushroom"),
-    Vocab("洋葱:ya²ngco¹ng:onion"),
-    Vocab("橙子:che²ngzi:orange"),
+    Vocab("洋葱:yang²cong¹:onion"),
+    Vocab("橙子:cheng²zi:orange"),
     Vocab("菠萝:bo¹luo²:pineapple"),
-    Vocab("土豆:tu³do⁴u:potato"),
-    Vocab("菠菜:bo¹ca⁴i:spinach"),
-    Vocab("草莓:ca³ome²i:strawberry"),
-    Vocab("西红柿:xi¹ho²ngshi⁴:tomato"),
+    Vocab("土豆:tu³dou⁴:potato"),
+    Vocab("菠菜:bo¹cai⁴:spinach"),
+    Vocab("草莓:cao³mei²:strawberry"),
+    Vocab("西红柿:xi¹hong²shi⁴:tomato"),
     Vocab("西瓜:xi¹gua¹:watermelon")
   )
 
@@ -114,5 +114,4 @@ object Lesson {
     Vocab("金色:jin¹se⁴:gold"),
     Vocab("银色:yin²se⁴:silver")
   )
-
 }
