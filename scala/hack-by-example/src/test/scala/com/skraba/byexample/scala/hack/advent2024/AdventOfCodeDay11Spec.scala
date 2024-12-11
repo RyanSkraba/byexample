096c0397bf83c2d6d4baab50b385082198d1b46f
Ryan Skraba <ryan@skraba.com>
2024-12-11 10:39:24 +0100

    feat: Add Advent of Code 2024 Day 11

--- scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2024/AdventOfCodeDay11Spec.scala
+++ scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2024/AdventOfCodeDay11Spec.scala
@@ -7,11 +7,13 @@ import org.scalatest.matchers.should.Matchers
 
 /** =Advent of Code 2024 Day 11 Solutions in scala=
   *
-  * Input:
+  * Input: A series of numbers (written on stones). Every time you blink, the number changes: if it is zero, it becomes
+  * 1, otherwise if it has an even number of digits, it splits exactly into two numbers, otherwise it multiplies by
+  * 2024.
   *
-  * Part 1:
+  * Part 1: After 25 blinks, how many stones do you have?
   *
-  * Part 2:
+  * Part 2: After 75 blinks, how many stones do you have?
   *
   * @see
   *   Rephrased from [[https://adventofcode.com/2024/day/11]]
@@ -20,42 +22,51 @@ class AdventOfCodeDay11Spec extends AnyFunSpecLike with Matchers with BeforeAndA
 
   object Solution {
 
-    case class ABC(a: Long) {}
+    def parse(in: String): Map[Long, Long] = in.split("\\s+").map(_.toLong -> 1L).toMap
 
-    def parse(in: String): Option[ABC] = None
+    def blink(in: Map[Long, Long]): Map[Long, Long] = {
+      in.toSeq
+        .flatMap {
+          case (0, count) => Seq(1L -> count)
+          case (n, count) if (n.toString.length % 2 == 0) =>
+            n.toString.splitAt(n.toString.length / 2) match {
+              case (n1, n2) => Seq(n1.toLong -> count, n2.toLong -> count)
+            }
+          case (n, count) => Seq(n * 2024L -> count)
+        }
+        .groupMapReduce(_._1)(_._2)(_ + _)
+    }
 
-    def part1(in: String*): Long = 100
+    def part1(in: String*): Long = LazyList.iterate(parse(in.head)) { blink }.drop(25).head.values.sum
 
-    def part2(in: String*): Long = 200
+    def part2(in: String*): Long = LazyList.iterate(parse(in.head)) { blink }.drop(75).head.values.sum
   }
 
   import Solution._
 
   describe("Example case") {
-    val input =
-      """
-        |""".trim.stripMargin.split("\n")
+    val input = "125 17"
 
     it("should match the puzzle description for part 1") {
-      part1(input: _*) shouldBe 100
+      part1(input) shouldBe 55312
     }
 
     it("should match the puzzle description for part 2") {
-      part2(input: _*) shouldBe 200
+      part2(input) shouldBe 65601038650482L
     }
   }
 
   describe("🔑 Solution 🔑") {
-    lazy val input = puzzleInput("Day11Input.txt")
-    lazy val answer1 = decryptLong("tTNGygZ0+O4PEH+5IiCrBw==")
-    lazy val answer2 = decryptLong("U9BZNCixKWAgOXNrGyDe5A==")
+    lazy val input = puzzleInput("Day11Input.txt").mkString("")
+    lazy val answer1 = decryptLong("i1CZ9HOq154JpruumX+xNQ==")
+    lazy val answer2 = decryptLong("eEXY2XYSbeZuNtK/0xHLWQ==")
 
     it("should have answers for part 1") {
-      part1(input: _*) shouldBe answer1
+      part1(input) shouldBe answer1
     }
 
     it("should have answers for part 2") {
-      part2(input: _*) shouldBe answer2
+      part2(input) shouldBe answer2
     }
   }
 }
