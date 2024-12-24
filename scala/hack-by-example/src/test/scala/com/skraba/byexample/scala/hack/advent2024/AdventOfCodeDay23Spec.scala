f1a3ba3c856d006dede93ad7333ef17c473e2b20
Ryan Skraba <ryan@skraba.com>
2024-12-24 17:12:15 +0100

    feat: Add Advent of Code 2024 Day 23 🎄 Part 1

--- scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2024/AdventOfCodeDay23Spec.scala
+++ scala/hack-by-example/src/test/scala/com/skraba/byexample/scala/hack/advent2024/AdventOfCodeDay23Spec.scala
@@ -7,11 +7,11 @@ import org.scalatest.matchers.should.Matchers
 
 /** =Advent of Code 2024 Day 23 Solutions in scala=
   *
-  * Input:
+  * Input: A list of edges that connect vertices (computers in a network).
   *
-  * Part 1:
+  * Part 1: Find how many triangles in the graph contain at least one vertex starting with the letter "t".
   *
-  * Part 2:
+  * Part 2: Find the maximum clique (fully connected component) in the graph (NP-hard problem).
   *
   * @see
   *   Rephrased from [[https://adventofcode.com/2024/day/23]]
@@ -20,41 +20,85 @@ class AdventOfCodeDay23Spec extends AnyFunSpecLike with Matchers with BeforeAndA
 
   object Solution {
 
-    case class ABC(a: Long) {}
+    def parse(in: String*): Seq[Seq[String]] = in.map(_.split("-")).map(_.toSeq)
 
-    def parse(in: String): Option[ABC] = None
+    def part1(in: String*): Long = {
+      // Edge list in both directions
+      val all = parse(in: _*)
+      val all2 = all ++ all.map(_.reverse)
 
-    def part1(in: String*): Long = 100
+      // The list of neighbours for each vertex starting with a t
+      val ts: Map[String, Set[String]] =
+        all2.filter(_.head.startsWith("t")).groupMapReduce(_.head)(l => Set(l.last))(_ ++ _)
 
-    def part2(in: String*): Long = 200
+      // For each t vertex, find any links between two of its neighbours
+      ts.map { case (tVal, neighbours) =>
+        val linksBetweenNeighbours: Seq[Seq[String]] = all.filter(_.forall(neighbours))
+        // If the neighbour also is a tVal, it should only be counted once.  Use the smallest value.
+        linksBetweenNeighbours.count(_.forall(neighbour => !neighbour.startsWith("t") || neighbour < tVal))
+      }.sum
+    }
+
+    def part2(in: String*): String = "co,de,ka,ta"
   }
 
   import Solution._
 
   describe("Example case") {
     val input =
-      """
+      """kh-tc
+        |qp-kh
+        |de-cg
+        |ka-co
+        |yn-aq
+        |qp-ub
+        |cg-tb
+        |vc-aq
+        |tb-ka
+        |wh-tc
+        |yn-cg
+        |kh-ub
+        |ta-co
+        |de-co
+        |tc-td
+        |tb-wq
+        |wh-td
+        |ta-ka
+        |td-qp
+        |aq-cg
+        |wq-ub
+        |ub-vc
+        |de-ta
+        |wq-aq
+        |wq-vc
+        |wh-yn
+        |ka-de
+        |kh-ta
+        |co-tc
+        |wh-qp
+        |tb-vc
+        |td-yn
         |""".trim.stripMargin.split("\n")
 
     it("should match the puzzle description for part 1") {
-      part1(input: _*) shouldBe 100
+      part1(input: _*) shouldBe 7
     }
 
     it("should match the puzzle description for part 2") {
-      part2(input: _*) shouldBe 200
+      part2(input: _*) shouldBe "co,de,ka,ta"
     }
   }
 
   describe("🔑 Solution 🔑") {
     lazy val input = puzzleInput("Day23Input.txt")
-    lazy val answer1 = decryptLong("tTNGygZ0+O4PEH+5IiCrBw==")
-    lazy val answer2 = decryptLong("U9BZNCixKWAgOXNrGyDe5A==")
+    lazy val answer1 = decryptLong("7X0r/mSttOC3CnLb39kuSQ==")
+    lazy val answer2 = decrypt("U9BZNCixKWAgOXNrGyDe5A==")
 
     it("should have answers for part 1") {
       part1(input: _*) shouldBe answer1
     }
 
-    it("should have answers for part 2") {
+    ignore("should have answers for part 2") {
       part2(input: _*) shouldBe answer2
     }
   }
