8bc4ede2c92603351354fab58067bb4c0e964c8a
Ryan Skraba <ryan@skraba.com>
2025-02-08 06:29:34 +0100

    test(markd,beautify): Improve tests

--- scala/markd-by-example/src/test/scala/com/skraba/byexample/scala/markd/BeautifyTaskSpec.scala
+++ scala/markd-by-example/src/test/scala/com/skraba/byexample/scala/markd/BeautifyTaskSpec.scala
@@ -1,12 +1,19 @@
 package com.skraba.byexample.scala.markd
 
 import com.skraba.docoptcli.DocoptCliGoSpec
-import org.docopt.DocoptExitException
+
+import scala.reflect.io.{Directory, File}
 
 /** Unit tests for [[BeautifyTask]] */
 class BeautifyTaskSpec extends DocoptCliGoSpec(MarkdGo, Some(BeautifyTask)) {
 
-  // TODO: Improve all unit tests
+  /** A local temporary directory for test file storage. */
+  val Tmp: Directory = Directory.makeTemp(getClass.getSimpleName)
+
+  /** Delete temporary resources after the script. */
+  override protected def afterAll(): Unit =
+    try { Tmp.deleteRecursively() }
+    catch { case ex: Exception => ex.printStackTrace() }
 
   describe(s"${Cli.Cli} $TaskCmd command line") {
 
@@ -17,4 +24,39 @@ class BeautifyTaskSpec extends DocoptCliGoSpec(MarkdGo, Some(BeautifyTask)) {
     itShouldThrowOnMissingOpt(Seq.empty)
   }
 
+  describe(s"${Cli.Cli} $TaskCmd beautify basic scenario") {
+
+    /** Generate a scenario with some files to beautify. */
+    def scenario(tag: String): Directory = {
+      val scenario = (Tmp / tag).createDirectory()
+      // A very simple file
+      File(scenario / "basic.md").writeAll(
+        s"""# Header
+           |Some text
+           |""".stripMargin
+      )
+      // The same file as it would be beautified
+      File(scenario / "basic_expected.md").writeAll(
+        s"""Header
+           |=====================================================================
+           |
+           |Some text
+           |""".stripMargin
+      )
+      scenario
+    }
+
+    it("should beautify a single file") {
+      val basic = scenario("basic")
+
+      (basic / "basic.md").toFile.slurp() shouldNot be((basic / "basic_expected.md").toFile.slurp())
+
+      withGoMatching(TaskCmd, basic) { case (stdout, stderr) =>
+        stderr shouldBe empty
+        stdout shouldBe ""
+      }
+
+      (basic / "basic.md").toFile.slurp() shouldBe (basic / "basic_expected.md").toFile.slurp()
+    }
+  }
 }
