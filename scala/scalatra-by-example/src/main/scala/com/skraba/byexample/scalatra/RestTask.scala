40d38d236279ebd0c1587f983616e990ad462393
Ryan Skraba <ryan@skraba.com>
2025-04-18 07:39:20 +0200

    chore(scalatra): Use an in-memory variable as a fake database

--- scala/scalatra-by-example/src/main/scala/com/skraba/byexample/scalatra/RestTask.scala
+++ scala/scalatra-by-example/src/main/scala/com/skraba/byexample/scalatra/RestTask.scala
@@ -3,6 +3,8 @@ package com.skraba.byexample.scalatra
 import com.skraba.byexample.scalatra.ScalatraGo.TestableServlet
 import com.skraba.docoptcli.DocoptCliGo.Task
 
+import scala.collection.mutable
+
 /** Command-line driver that launches a server that has a basic REST API. */
 object RestTask extends Task {
 
@@ -24,28 +26,27 @@ object RestTask extends Task {
 
   def go(opts: TaskOptions): Unit = ScalatraGo.runStandaloneServer(opts.getInt("--port", 8080), classOf[Srvlet])
 
+  case class Product(id: Int, name: String) {
+    // TODO: JSON binding maybe
+    lazy val json: String = s"""{"id": $id, "name": "$name"}"""
+  }
+
+  val db: mutable.Map[Int, Product] = mutable.SortedMap(1 -> Product(1, "one"), 2 -> Product(2, "two"))
+
   class Srvlet extends TestableServlet {
 
     before() {
       contentType = "application/json"
     }
 
-    // TODO: Do a better in-memory DB
-    // TODO: JSON binding maybe
-
     get("/product/") {
-      """[
-        |  {"id": 1, "name": "one"},
-        |  {"id": 2, "name": "two"}
-        |]""".stripMargin
+      db.values.map("  " + _.json).mkString("[\n", ",\n", "\n]")
     }
 
     get("/product/:id") {
-      params("id") match {
-        case "1"   => """{"id": 1, "name": "one"}"""
-        case "2"   => """{"id": 2, "name": "two"}"""
-        case other => halt(404, s"Product $other not found")
-      }
+      val paramId = params("id")
+      val id = paramId.toIntOption.getOrElse(halt(404, s"Product $paramId not found"))
+      db.getOrElse(id, halt(404, s"Product $paramId not found")).json
     }
   }
 }
