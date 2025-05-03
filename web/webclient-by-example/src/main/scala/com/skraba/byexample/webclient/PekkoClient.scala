3be4cb09750db6c02a6cd1539c19229e5482067a
Ryan Skraba <ryan@skraba.com>
2025-05-03 07:44:28 +0200

    refactor: Refactor Pekko client code to implement all methods

--- web/webclient-by-example/src/main/scala/com/skraba/byexample/webclient/PekkoClient.scala
+++ web/webclient-by-example/src/main/scala/com/skraba/byexample/webclient/PekkoClient.scala
@@ -19,14 +19,23 @@ object PekkoClient extends WebClientGo.SimpleClient {
       pekko.stdout-loglevel = "WARNING"
       """)
 
-  /** Make a GET request to the server. */
-  override def get(path: String): SimpleResponse = {
+  /** Makes a single HTTP request using the Pekko actor system.
+    * @param method
+    *   The HTTP method to use
+    * @param path
+    *   A string representation of the path to request
+    * @param entity
+    *   The body (if any) to use for the request.
+    * @return
+    *   A SimpleResponse with the status code and response text.
+    */
+  private def request(method: HttpMethod, path: String, entity: RequestEntity = HttpEntity.Empty): SimpleResponse = {
     implicit val system: ActorSystem = ActorSystem("my", pekkoNoLogging)
     import system.dispatcher
 
     val get = Await.result(
       Http()
-        .singleRequest(HttpRequest(method = HttpMethods.GET, uri = new URL(path).toString))
+        .singleRequest(HttpRequest(method = method, uri = new URL(path).toString, entity = entity))
         .flatMap(response => response.entity.toStrict(10.seconds).map(response.status -> _.data.utf8String)),
       Duration.Inf
     )
@@ -35,27 +44,18 @@ object PekkoClient extends WebClientGo.SimpleClient {
     Await.result(system.terminate(), Duration.Inf)
     result
   }
+
+  /** Make a GET request to the server. */
+  override def get(path: String): SimpleResponse = request(HttpMethods.GET, path)
 
   /** Make a POST request to the server. */
-  override def post(path: String, payload: String): SimpleResponse = {
-    implicit val system: ActorSystem = ActorSystem("my", pekkoNoLogging)
-    import system.dispatcher
-
-    val get = Await.result(
-      Http()
-        .singleRequest(HttpRequest(method = HttpMethods.POST, uri = new URL(path).toString, entity = HttpEntity(ByteString(payload))))
-        .flatMap(response => response.entity.toStrict(10.seconds).map(response.status -> _.data.utf8String)),
-      Duration.Inf
-    )
-
-    val result = SimpleResponse(get._1.intValue(), get._2)
-    Await.result(system.terminate(), Duration.Inf)
-    result
-  }
+  override def post(path: String, payload: String): SimpleResponse =
+    request(HttpMethods.POST, path, HttpEntity(ByteString(payload)))
 
   /** Make a PUT request to the server. */
-  override def put(path: String, payload: String): SimpleResponse = ???
+  override def put(path: String, payload: String): SimpleResponse =
+    request(HttpMethods.PUT, path, HttpEntity(ByteString(payload)))
 
   /** Make a DELETE request to the server. */
-  override def delete(path: String): SimpleResponse = ???
+  override def delete(path: String): SimpleResponse = request(HttpMethods.DELETE, path)
 }
